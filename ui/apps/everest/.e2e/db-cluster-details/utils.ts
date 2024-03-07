7d97e00fe7ad215e8d0c02f4c80e0dd0ee6b702a
Fabio Silva <ffjs1993@gmail.com>
2024-03-07 17:01:19 +0000

    fix: use correct selector for E2E test

--- ui/apps/everest/.e2e/db-cluster-details/utils.ts
+++ ui/apps/everest/.e2e/db-cluster-details/utils.ts
@@ -1,17 +1,21 @@
-import { expect } from '@playwright/test';
+import { Page, expect } from '@playwright/test';
 
-export const clickCreateSchedule = async (page) => {
+export const clickCreateSchedule = async (page: Page) => {
   const createBackupButton = await page.getByTestId('menu-button');
   createBackupButton.click();
   const scheduleMenuItem = await page.getByTestId('schedule-menu-item');
   await scheduleMenuItem.click();
-  await expect(page.getByText('Create Schedule')).toBeVisible();
+  await expect(
+    page.getByRole('heading').filter({ hasText: 'Create Schedule' })
+  ).toBeVisible();
 };
 
-export const clickOnDemandBackup = async (page) => {
+export const clickOnDemandBackup = async (page: Page) => {
   const createBackupButton = await page.getByTestId('menu-button');
   createBackupButton.click();
   const onDemandMenuItem = await page.getByTestId('now-menu-item');
   await onDemandMenuItem.click();
-  await expect(page.getByText('Create Backup')).toBeVisible();
+  await expect(
+    page.getByRole('heading').filter({ hasText: 'Create Backup' })
+  ).toBeVisible();
 };
