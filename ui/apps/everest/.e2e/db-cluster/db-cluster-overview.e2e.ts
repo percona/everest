e520311a3e5c64cd8a5ae763c9b5bbd5d3d12517
Iaroslavna Soloveva <90199600+solovevayaroslavna@users.noreply.github.com>
2024-03-14 11:59:35 +0400

    [UI] Everest 871 delete confirmation (#83)

--- ui/apps/everest/.e2e/db-cluster/db-cluster-overview.e2e.ts
+++ ui/apps/everest/.e2e/db-cluster/db-cluster-overview.e2e.ts
@@ -58,4 +58,39 @@ test.describe('DB Cluster Overview', async () => {
         .getByText('Enabled')
     ).toBeVisible();
   });
+
+  test('Delete Action', async ({ page, request }) => {
+    const dbName = 'delete-test';
+
+    await createDbClusterFn(request, {
+      dbName: dbName,
+      dbType: 'mysql',
+      numberOfNodes: '1',
+    });
+
+    const row = page.getByText(dbName);
+    await row.click();
+
+    await expect(
+      page.getByRole('heading', {
+        name: dbName,
+      })
+    ).toBeVisible();
+
+    const actionButton = page.getByTestId('actions-button');
+    await actionButton.click();
+
+    const deleteButton = page.getByTestId(`${dbName}-delete`);
+    await deleteButton.click();
+
+    await page.getByTestId(`${dbName}-form-dialog`).waitFor();
+    await expect(page.getByTestId('irreversible-action-alert')).toBeVisible();
+    const deleteConfirmationButton = page
+      .getByRole('button')
+      .filter({ hasText: 'Delete' });
+    await expect(deleteConfirmationButton).toBeDisabled();
+    await page.getByTestId('text-input-confirm-input').fill(dbName);
+    await expect(deleteConfirmationButton).toBeEnabled();
+    await deleteConfirmationButton.click();
+  });
 });
