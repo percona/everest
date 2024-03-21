a72d9223736130611c3c9c31dce9bfa26d5c3488
Filip Mikes <filip.mikes@3pillarglobal.com>
2024-03-21 10:39:26 +0100

    EVEREST-863 Wrapped whole app to ErrorBoundary

--- ui/apps/everest/src/components/main/Main.tsx
+++ ui/apps/everest/src/components/main/Main.tsx
@@ -45,57 +45,57 @@ export const Main = () => {
     : `calc(${theme.spacing(7)} + 1px)`;
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <AppBar />
-      <Drawer />
-      <Box
-        component="main"
-        sx={{
-          padding: 4,
-          width:
-            activeBreakpoint === 'mobile'
-              ? '100%'
-              : `calc(100% - ${drawerWidth})`,
-        }}
-      >
-        <Toolbar />
-        {isFetching ? (
-          <>
-            <Skeleton variant="rectangular" />
-            <Skeleton variant="rectangular" />
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
-            <Skeleton variant="rectangular" />
-          </>
-        ) : isError ? (
-          <Stack alignItems="center">
-            <Typography variant="subtitle1">
-              {Messages.somethingWrong}
-            </Typography>
-            <Button
-              onClick={handleClick}
-              variant="outlined"
-              endIcon={<ReplayIcon />}
-              sx={{ mt: 1 }}
-            >
-              {Messages.retry}
-            </Button>
-          </Stack>
-        ) : (
-          <ErrorContextProvider>
-            <ErrorBoundary fallback={<GenericError />}>
+    <ErrorContextProvider>
+      <ErrorBoundary fallback={<GenericError />}>
+        <Box sx={{ display: 'flex' }}>
+          <AppBar />
+          <Drawer />
+          <Box
+            component="main"
+            sx={{
+              padding: 4,
+              width:
+                activeBreakpoint === 'mobile'
+                  ? '100%'
+                  : `calc(100% - ${drawerWidth})`,
+            }}
+          >
+            <Toolbar />
+            {isFetching ? (
+              <>
+                <Skeleton variant="rectangular" />
+                <Skeleton variant="rectangular" />
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
+                <Skeleton variant="rectangular" />
+              </>
+            ) : isError ? (
+              <Stack alignItems="center">
+                <Typography variant="subtitle1">
+                  {Messages.somethingWrong}
+                </Typography>
+                <Button
+                  onClick={handleClick}
+                  variant="outlined"
+                  endIcon={<ReplayIcon />}
+                  sx={{ mt: 1 }}
+                >
+                  {Messages.retry}
+                </Button>
+              </Stack>
+            ) : (
               <Outlet />
-            </ErrorBoundary>
-          </ErrorContextProvider>
-        )}
-        {openWelcomeDialogLS && (
-          <WelcomeDialog
-            open={openWelcomeDialogLS}
-            closeDialog={handleCloseWelcomeDialog}
-          />
-        )}
-      </Box>
-    </Box>
+            )}
+            {openWelcomeDialogLS && (
+              <WelcomeDialog
+                open={openWelcomeDialogLS}
+                closeDialog={handleCloseWelcomeDialog}
+              />
+            )}
+          </Box>
+        </Box>
+      </ErrorBoundary>
+    </ErrorContextProvider>
   );
 };
