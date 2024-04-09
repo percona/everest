a88ceaab0b68df3effccbc47437d7beaedc0df99
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2024-04-09 10:24:11 +0400

    EVEREST-924: expand label-content component

--- ui/packages/ui-lib/src/form/inputs/text-array/text-array.tsx
+++ ui/packages/ui-lib/src/form/inputs/text-array/text-array.tsx
@@ -1,17 +1,25 @@
+// everest
+// Copyright (C) 2023 Percona LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import { FieldError, useFieldArray, useFormContext } from 'react-hook-form';
-import {
-  Button,
-  IconButton,
-  InputAdornment,
-  Stack,
-  Typography,
-} from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
+import { IconButton, InputAdornment } from '@mui/material';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 
 import { TextArrayProps } from './text-array.types';
-import { Messages } from './text-array.messages';
 import TextInput from '../text';
+import LabeledContent from "../../../labeled-content";
 
 const TextArray = ({
   fieldName,
@@ -35,66 +43,49 @@ const TextArray = ({
 
   return (
     <>
-      <Stack direction="row">
-        {label && (
-          <Typography
-            // @ts-ignore
-            variant="sectionHeading"
-            sx={{ mt: 4, mb: 0.5 }}
-          >
-            {label}
-          </Typography>
-        )}
-        <Button
-          variant="text"
-          size="medium"
-          data-testid="add-text-input-button"
-          startIcon={<AddIcon />}
-          sx={{
-            width: 'fit-content',
-            alignSelf: 'end',
-            ml: 'auto',
-          }}
-          onClick={() => {
+      <LabeledContent
+        label={label}
+        actionButtonProps={{
+          dataTestId: 'add-text-input-button',
+          onClick: () => {
             append({
               [fieldKey]: '',
             });
-          }}
-        >
-          {Messages.addNew}
-        </Button>
-      </Stack>
-      {defaultFields.map((field, index) => (
-        <TextInput
-          control={control}
-          name={`${fieldName}.${index}.${fieldKey}`}
-          key={`${fieldName}_${field.id}`}
-          textFieldProps={{
-            variant: 'outlined',
-            placeholder,
-            error: !!error(index),
-            helperText: error(index) ? error(index)?.message : '',
-            sx: {
-              width: '100%',
-              '&:not(:last-child)': {
-                mb: 1,
+          },
+        }}
+      >
+        {defaultFields.map((field, index) => (
+          <TextInput
+            control={control}
+            name={`${fieldName}.${index}.${fieldKey}`}
+            key={`${fieldName}_${field.id}`}
+            textFieldProps={{
+              variant: 'outlined',
+              placeholder,
+              error: !!error(index),
+              helperText: error(index) ? error(index)?.message : '',
+              sx: {
+                width: '100%',
+                '&:not(:last-child)': {
+                  mb: 1,
+                },
               },
-            },
-            InputProps: {
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    data-testid={`delete-text-input-${index}-button`}
-                    onClick={() => remove(index)}
-                  >
-                    <DeleteOutlineOutlinedIcon />
-                  </IconButton>
-                </InputAdornment>
-              ),
-            },
-          }}
-        />
-      ))}
+              InputProps: {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      data-testid={`delete-text-input-${index}-button`}
+                      onClick={() => remove(index)}
+                    >
+                      <DeleteOutlineOutlinedIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              },
+            }}
+          />
+        ))}
+      </LabeledContent>
     </>
   );
 };
--- ui/packages/ui-lib/src/labeled-content/labeled-content.tsx
+++ ui/packages/ui-lib/src/labeled-content/labeled-content.tsx
@@ -1,25 +1,64 @@
-import { Typography } from '@mui/material';
+// everest
+// Copyright (C) 2023 Percona LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Button, Stack, Typography } from '@mui/material';
 import { LabeledContentProps } from './labeled-content.types';
+import AddIcon from '@mui/icons-material/Add';
 
 const LabeledContent = ({
   label,
   children,
   isRequired = false,
   sx,
+  actionButtonProps,
   ...typographyProps
-}: LabeledContentProps) => (
-  <>
-    <Typography
-      // @ts-ignore
-      variant="sectionHeading"
-      sx={{ mt: 2, mb: 0.5, ...sx }}
-      {...typographyProps}
-    >
-      {label}
-      {isRequired && <span>*</span>}
-    </Typography>
-    {children}
-  </>
-);
+}: LabeledContentProps) => {
+  const { dataTestId, buttonText } = actionButtonProps || {};
+
+  return (
+    <>
+      <Stack direction="row">
+        <Typography
+          // @ts-ignore
+          variant="sectionHeading"
+          sx={{ mt: 2, mb: 0.5, ...sx }}
+          {...typographyProps}
+        >
+          {label}
+          {isRequired && <span>*</span>}
+        </Typography>
+        {actionButtonProps && (
+          <Button
+            variant="text"
+            size="medium"
+            startIcon={<AddIcon />}
+            sx={{
+              width: 'fit-content',
+              alignSelf: 'end',
+              ml: 'auto',
+            }}
+            data-testid={dataTestId || 'labeled-content-action-button'}
+            {...actionButtonProps}
+          >
+            {buttonText || 'Add new'}
+          </Button>
+        )}
+      </Stack>
+      {children}
+    </>
+  );
+};
 
 export default LabeledContent;
