1d69f6b8d45a5c91aaf166618f39c2577e28a685
Michal Kralik <michal.kralik@percona.com>
2024-04-10 11:00:43 +0200

    Linters

--- api/validation_test.go
+++ api/validation_test.go
@@ -956,7 +956,6 @@ func TestValidateBucketName(t *testing.T) {
 	}
 
 	for _, tc := range tcases {
-		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 			err := validateBucketName(tc.input)
