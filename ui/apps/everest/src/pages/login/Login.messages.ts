e478dbdddccde92b4059411cf7f0fc77d76ede35
Diogo Recharte <diogo.recharte@percona.com>
2024-04-11 18:41:40 +0100

    Update login page to use username/password instead of token

--- ui/apps/everest/src/pages/login/Login.messages.ts
+++ ui/apps/everest/src/pages/login/Login.messages.ts
@@ -4,10 +4,8 @@ export const Messages = {
   intro: `Percona Everest accelerates code deployment and scaling, and reduces database administration overhead.
     With the help of Percona Everest, you can regain control over data access, database configuration, and cloud database costs. Provision your first database cluster with Percona Everest today!`,
   login: 'Log in',
-  insertToken:
-    'Enter the authorization token that you received during the installation process.',
-  resetToken: 'Reset token',
-  useTerminal: 'Reset your authorization token using the Everest CLI.',
+  insertCredentials: 'Enter your username and password to log in.',
   ok: 'OK',
-  token: 'Token',
+  username: 'Username',
+  password: 'Password',
 };
