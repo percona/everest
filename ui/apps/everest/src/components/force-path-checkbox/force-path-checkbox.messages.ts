75cfe832ec16a7408e563309f9beb21b80baef6b
Diogo Recharte <diogo.recharte@percona.com>
2024-04-12 10:45:40 +0100

    [UI] EVEREST-919 Add force path style option (#189)
    * feat: add forcePathStyle checkbox and tooltip
    
    * Fix linting

---
+++ ui/apps/everest/src/components/force-path-checkbox/force-path-checkbox.messages.ts
@@ -0,0 +1,5 @@
+export const Messages = {
+  forcePathStyle: 'Force path-style URL access',
+  tooltip:
+    "Some storage providers require path-style URLs to access the bucket. Read the provider's documentation to determine if this is required.",
+};
