4084ccc7c8d887aa56f6c2e207a3717f02f149e9
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2024-05-03 11:27:04 +0400

    EVEREST-994-storybook: format

--- ui/packages/ui-lib/src/form/inputs/radio-group/radio-group.stories.tsx
+++ ui/packages/ui-lib/src/form/inputs/radio-group/radio-group.stories.tsx
@@ -5,9 +5,9 @@ import RadioGroup from './radio-group';
 import { RadioGroupProps } from './radio-group.types';
 
 type ExpandedRadioGroupProps = RadioGroupProps & {
-  customSize: number,
-  required: boolean,
-}
+  customSize: number;
+  required: boolean;
+};
 
 const meta = {
   title: 'RadioGroup',
@@ -15,70 +15,65 @@ const meta = {
   parameters: {
     layout: 'centered',
   },
-  render: function Render({
-    required,
-    customSize,
-    options,
-    ...args
-  }) {
+  render: function Render({ required, customSize, options, ...args }) {
     const methods = useForm();
 
     return (
-        <FormProvider {...methods}>
-          <RadioGroup 
-            {...args}
-            options={options} 
-            name='RadioGroup1'
-            label='Small size'
-            isRequired={required}
-            radioGroupFieldProps={{
-              sx: {
-                '& .MuiSvgIcon-root': {
-                  fontSize: 20,
-                },
+      <FormProvider {...methods}>
+        <RadioGroup
+          {...args}
+          options={options}
+          name="RadioGroup1"
+          label="Small size"
+          isRequired={required}
+          radioGroupFieldProps={{
+            sx: {
+              '& .MuiSvgIcon-root': {
+                fontSize: 20,
               },
-            }}
-            controllerProps={{
-                defaultValue: 'First'
-            }}
-          />
+            },
+          }}
+          controllerProps={{
+            defaultValue: 'First',
+          }}
+        />
 
-          <RadioGroup 
-            {...args}
-            options={options} 
-            name='RadioGroup2'
-            label='Medium size'
-            isRequired={required}
-            radioGroupFieldProps={{
-              sx: {
-                '& .MuiSvgIcon-root': {
-                  fontSize: 24,
-                },
-              }
-            }}
-            controllerProps={{
-                defaultValue: 'First'
-            }}
-          />
+        <RadioGroup
+          {...args}
+          options={options}
+          name="RadioGroup2"
+          label="Medium size"
+          isRequired={required}
+          radioGroupFieldProps={{
+            sx: {
+              '& .MuiSvgIcon-root': {
+                fontSize: 24,
+              },
+            },
+          }}
+          controllerProps={{
+            defaultValue: 'First',
+          }}
+        />
 
-          <RadioGroup
-            {...args}
-            options={options} 
-            name='RadioGroup3'
-            label='Custom size(the default size is large)'
-            isRequired={required}
-            radioGroupFieldProps={{
-              sx: {
-                '& .MuiSvgIcon-root': {
-                  fontSize: customSize,
-                },
-              }
-            }}
-            controllerProps={{
-                defaultValue: 'First'
-            }}
-          />
-        </FormProvider>
+        <RadioGroup
+          {...args}
+          options={options}
+          name="RadioGroup3"
+          label="Custom size(the default size is large)"
+          isRequired={required}
+          radioGroupFieldProps={{
+            sx: {
+              '& .MuiSvgIcon-root': {
+                fontSize: customSize,
+              },
+            },
+          }}
+          controllerProps={{
+            defaultValue: 'First',
+          }}
+        />
+      </FormProvider>
     );
   },
 } satisfies Meta<ExpandedRadioGroupProps>;
@@ -89,10 +84,10 @@ type Story = StoryObj<ExpandedRadioGroupProps>;
 export const Basic: Story = {
   args: {
     options: [
-        {label: 'First', value: 'First', disabled: false}, 
-        {label: 'Second', value: 'Second', disabled: false}, 
-        {label: 'Third', value: 'Third', disabled: false}, 
-        {label: 'Fourth', value: 'Fourth', disabled: true}
+      { label: 'First', value: 'First', disabled: false },
+      { label: 'Second', value: 'Second', disabled: false },
+      { label: 'Third', value: 'Third', disabled: false },
+      { label: 'Fourth', value: 'Fourth', disabled: true },
     ],
     customSize: 28,
     required: false,
