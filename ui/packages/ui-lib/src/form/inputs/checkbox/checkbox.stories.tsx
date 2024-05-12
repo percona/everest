356d44b168cafc9d710080c3aa722e2a015b359b
Danila <officialdannisimo@gmail.com>
2024-05-13 02:34:23 +0400

    EVEREST-1039: add more variants

--- ui/packages/ui-lib/src/form/inputs/checkbox/checkbox.stories.tsx
+++ ui/packages/ui-lib/src/form/inputs/checkbox/checkbox.stories.tsx
@@ -2,11 +2,13 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { FormProvider, useForm } from 'react-hook-form';
 import Checkbox from './checkbox';
 import { CheckboxProps } from './checkbox.types';
+import { Stack } from '@mui/material';
 
 type CustomArgs = CheckboxProps & {
   disabled?: boolean;
   size?: 'small' | 'medium';
   required?: boolean;
+  indeterminate?: boolean
 };
 
 const meta = {
@@ -30,19 +32,68 @@ const meta = {
     required: {
       type: 'boolean',
     },
+    indeterminate: {
+      type: 'boolean',
+    },
   },
 
-  render: function Render({ label, disabled, size, required }) {
+  render: function Render({ label, disabled, size, required, indeterminate }) {
     const methods = useForm();
 
     return (
       <FormProvider {...methods}>
-        <Checkbox
-          name="checkbox"
-          label={label}
-          checkboxProps={{ disabled, size }}
-          labelProps={{ isRequired: required }}
-        />
+        <Stack direction={'row'} columnGap={'20px'} justifyContent={'center'}>
+          <Checkbox
+            name="checkbox"
+            label="small"
+            checkboxProps={{ size: 'small' }}
+          />
+          <Checkbox
+            name="checkbox"
+            label="checked"
+            checkboxProps={{ size: 'small', defaultChecked: true }}
+          />
+          <Checkbox
+            name="checkbox"
+            label="disabled"
+            checkboxProps={{ size: 'small', disabled: true }}
+          />
+          <Checkbox
+            name="checkbox"
+            label="indeterminate"
+            checkboxProps={{ size: 'small', indeterminate: true }}
+          />
+        </Stack>
+        <Stack direction={'row'} columnGap={'20px'} justifyContent={'center'}>
+          <Checkbox
+            name="checkbox"
+            label="medium"
+            checkboxProps={{ size: 'medium' }}
+          />
+          <Checkbox
+            name="checkbox"
+            label="checked"
+            checkboxProps={{ size: 'medium', defaultChecked: true }}
+          />
+          <Checkbox
+            name="checkbox"
+            label="disabled"
+            checkboxProps={{ size: 'medium', disabled: true }}
+          />
+          <Checkbox
+            name="checkbox"
+            label="indeterminate"
+            checkboxProps={{ size: 'medium', indeterminate: true }}
+          />
+        </Stack>
+        <Stack alignItems={'center'}>
+          <Checkbox
+            name="custom"
+            label={label}
+            checkboxProps={{ disabled, size, indeterminate }}
+            labelProps={{ isRequired: required }}
+          />
+        </Stack>
       </FormProvider>
     );
   },
@@ -54,9 +105,10 @@ type Story = StoryObj<CustomArgs>;
 
 export const Basic: Story = {
   args: {
-    label: 'Label',
+    label: 'Custom',
     disabled: false,
     size: 'medium',
     required: false,
+    indeterminate: false
   },
 };
