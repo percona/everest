c82f180815e30718ef5ddb46293ab09f21f42c5b
Mayank Shah <mayank.shah@percona.com>
2024-05-15 11:18:04 +0530

    Revert num workers

--- api-tests/playwright.config.ts
+++ api-tests/playwright.config.ts
@@ -30,9 +30,8 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 5,
+  retries: 2,
+  workers: process.env.CI ? 5 : 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['github'],
