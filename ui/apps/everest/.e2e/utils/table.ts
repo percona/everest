402bb96eb160f9953ce342fae389cafe438fa8f5
Fábio Silva <ffjs1993@gmail.com>
2024-05-16 10:04:53 +0100

    fix: e2e tentative (#299)

--- ui/apps/everest/.e2e/utils/table.ts
+++ ui/apps/everest/.e2e/utils/table.ts
@@ -1,4 +1,4 @@
-import { Page } from '@playwright/test';
+import { Page, expect } from '@playwright/test';
 
 export const findRowAndClickActions = async (
   page: Page,
@@ -15,4 +15,11 @@ export const findRowAndClickActions = async (
   if (nameOfAction) {
     await page.getByRole('menuitem', { name: nameOfAction }).click();
   }
+};
+
+// TODO remove after DB Cluster's PATCH method is implemented
+export const waitForInitializingState = async (page: Page, name: string) => {
+  const dbRow = page.getByRole('row').filter({ hasText: name });
+  await expect(dbRow).toBeVisible();
+  await expect(dbRow.getByText('Unknown')).not.toBeVisible({ timeout: 15000 });
 };
