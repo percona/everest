1d82dbab5f39296d79d09f08f2ff48b3678ac375
Mayank Shah <mayank.shah@percona.com>
2024-05-27 15:17:33 +0530

    Add set-password CLI command

---
+++ commands/accounts/set_password.go
@@ -0,0 +1,82 @@
+// everest
+// Copyright (C) 2023 Percona LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package accounts holds commands for accounts command.
+package accounts
+
+import (
+	"context"
+	"errors"
+	"net/url"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+
+	accountscli "github.com/percona/everest/pkg/accounts/cli"
+	"github.com/percona/everest/pkg/kubernetes"
+)
+
+// NewSetPwCommand returns a new set-password command.
+func NewSetPwCommand(l *zap.SugaredLogger) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "set-password",
+		Example: "everestctl accounts set-password --username user1 --password $USER_PASS",
+		Run: func(cmd *cobra.Command, args []string) { //nolint:revive
+			initSetPwViperFlags(cmd)
+
+			kubeconfigPath := viper.GetString("kubeconfig")
+			username := viper.GetString("username")
+			password := viper.GetString("password")
+
+			k, err := kubernetes.New(kubeconfigPath, l)
+			if err != nil {
+				var u *url.Error
+				if errors.As(err, &u) {
+					l.Error("Could not connect to Kubernetes. " +
+						"Make sure Kubernetes is running and is accessible from this computer/server.")
+				}
+				os.Exit(0)
+			}
+
+			cli := accountscli.New(l)
+			cli.WithAccountManager(k.Accounts())
+			if err != nil {
+				l.Error(err)
+				os.Exit(1)
+			}
+
+			if err := cli.SetPassword(context.Background(), username, password); err != nil {
+				l.Error(err)
+				os.Exit(1)
+			}
+		},
+	}
+	initSetPwFlags(cmd)
+	return cmd
+}
+
+func initSetPwFlags(cmd *cobra.Command) {
+	cmd.Flags().StringP("username", "u", "", "Username of the account")
+	cmd.Flags().StringP("password", "p", "", "Password of the account")
+}
+
+func initSetPwViperFlags(cmd *cobra.Command) {
+	viper.BindPFlag("username", cmd.Flags().Lookup("username"))     //nolint:errcheck,gosec
+	viper.BindPFlag("password", cmd.Flags().Lookup("password"))     //nolint:errcheck,gosec
+	viper.BindEnv("kubeconfig")                                     //nolint:errcheck,gosec
+	viper.BindPFlag("kubeconfig", cmd.Flags().Lookup("kubeconfig")) //nolint:errcheck,gosec
+}
