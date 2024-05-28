c31177c81b328cdd2733e1a7b2a6a7796cfff8f9
Fabio Silva <ffjs1993@gmail.com>
2024-05-28 09:32:46 +0100

    chore: show button for upgrading CR

--- ui/apps/everest/src/pages/settings/namespaces/namespace-details/cluster-status-table.tsx
+++ ui/apps/everest/src/pages/settings/namespaces/namespace-details/cluster-status-table.tsx
@@ -1,18 +1,9 @@
 import { useMemo } from 'react';
 import { MRT_ColumnDef } from 'material-react-table';
 import { Table } from '@percona/ui-lib';
-import {
-  DbUpgradePendingTask,
-  OperatorUpgradeDb,
-} from 'shared-types/dbEngines.types';
+import { OperatorUpgradeDb } from 'shared-types/dbEngines.types';
 import { ClusterStatusTableProps } from './types';
-
-const TASK_STATUS_MAP: Record<DbUpgradePendingTask, string> = {
-  notReady: 'Not ready',
-  ready: 'Ready',
-  restart: 'Restart',
-  upgradeEngine: 'Upgrade engine',
-};
+import { Button } from '@mui/material';
 
 const ClusterStatusTable = ({
   namespace,
@@ -25,14 +16,18 @@ const ClusterStatusTable = ({
         header: 'Database name',
       },
       {
-        accessorKey: 'pendingTask',
-        header: 'Pending task',
-        Cell: ({ cell }) =>
-          TASK_STATUS_MAP[cell.getValue<DbUpgradePendingTask>()],
-      },
-      {
         accessorKey: 'message',
-        header: 'Message',
+        header: 'Actions',
+        Cell: ({ cell, row }) => {
+          const task = row.original.pendingTask;
+          const message = cell.getValue<string>();
+
+          if (task === 'restart') {
+            return <Button>{message}</Button>;
+          }
+
+          return message;
+        },
       },
     ],
     []
