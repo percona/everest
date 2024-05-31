08ec1c1ffe5b42e4eb6c36cb3d7bbcc736b96ca5
Fabio Silva <ffjs1993@gmail.com>
2024-05-31 02:18:39 +0100

    chore: call Settings API

--- ui/apps/everest/src/api/everestConfigs.ts
+++ ui/apps/everest/src/api/everestConfigs.ts
@@ -2,15 +2,7 @@ import { EverestConfigPayload } from 'shared-types/configs.types';
 import { api } from './api';
 
 export const getEverestConfigs = async () => {
-  // const response = await api.get<EverestConfigPayload>('/settings');
+  const response = await api.get<EverestConfigPayload>('/settings');
 
-  // return response.data;
-
-  // TODO replace with API call
-  return {
-    oidc: {
-      clientId: '0oaes7mtfpYjxn2d81d7',
-      issuerURL: 'https://id-dev.percona.com/oauth2/default',
-    },
-  };
+  return response.data;
 };
