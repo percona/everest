3dbea4ccc8b3b3b7bea112015f6c20367644838b
Fabio Silva <ffjs1993@gmail.com>
2024-06-13 00:19:25 +0100

    fix: date-picker typing

--- ui/packages/ui-lib/src/form/inputs/date-time-picker/date-time-picker.types.ts
+++ ui/packages/ui-lib/src/form/inputs/date-time-picker/date-time-picker.types.ts
@@ -1,7 +1,8 @@
 import { Control, UseControllerProps } from 'react-hook-form';
 import { DateTimePickerProps } from '@mui/x-date-pickers/DateTimePicker';
+import { PickerValidDate } from '@mui/x-date-pickers';
 
-export interface DateTimePickerInputProps<T extends never>
+export interface DateTimePickerInputProps<T extends PickerValidDate>
   extends DateTimePickerProps<T> {
   control?: Control;
   controllerProps?: UseControllerProps;
