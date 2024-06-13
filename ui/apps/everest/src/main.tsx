780caa24b9bcceda407597cbec57f11f6c625fba
Fabio Silva <fabio@Fabios-MBP.home>
2024-06-13 18:26:34 +0100

    chore: suppress first-child warning

--- ui/apps/everest/src/main.tsx
+++ ui/apps/everest/src/main.tsx
@@ -1,4 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import App from 'App';
 
+// We don't use SSR, so we suppress this SSR annoying waring about first-child
+const consoleError = console.error;
+
+console.error = function filterErrors(msg, ...args) {
+    if (/server-side rendering/.test(msg)) {
+        return;
+    }
+    consoleError(msg, ...args);
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
