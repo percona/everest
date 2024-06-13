5a06d250b21b96fc928c405e704a1eb9a6601cd1
solovevayaroslavna <solovevayaroslavna@users.noreply.github.com>
2024-06-13 13:56:43 +0000

    chore: lint/format

--- ui/apps/everest/.e2e/utils/no-match.ts
+++ ui/apps/everest/.e2e/utils/no-match.ts
@@ -3,5 +3,5 @@ import { expect, Page } from '@playwright/test';
 export const checkNoMatchPage = async (page: Page) => {
   const button = page.getByTestId('no-match-button');
 
-  await expect(button).toBeVisible({timeout: 15000});
+  await expect(button).toBeVisible({ timeout: 15000 });
 };
