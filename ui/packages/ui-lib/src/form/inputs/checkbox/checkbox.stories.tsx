776226d3deb028cd7359ed6af891765285426fdf
Danila <officialdannisimo@gmail.com>
2024-06-15 08:00:38 +0400

    EVEREST-1039 add documentation

--- ui/packages/ui-lib/src/form/inputs/checkbox/checkbox.stories.tsx
+++ ui/packages/ui-lib/src/form/inputs/checkbox/checkbox.stories.tsx
@@ -1,99 +1,90 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { FormProvider, useForm } from 'react-hook-form';
 import Checkbox from './checkbox';
-import { CheckboxProps } from './checkbox.types';
-import { Stack } from '@mui/material';
+import * as DocBlock from '@storybook/blocks';
 
-type CustomArgs = CheckboxProps & {
+type CustomArgs = {
+  label?: string;
   disabled?: boolean;
   size?: 'small' | 'medium';
   required?: boolean;
   indeterminate?: boolean;
+  defaultChecked?: boolean;
 };
 
 const meta = {
   title: 'Checkbox',
-  component: Checkbox,
-  parameters: {
-    layout: 'centered',
-  },
-
+  tags: ['autodocs'],
   argTypes: {
     label: {
+      defaultValue: '',
       type: 'string',
     },
     disabled: {
+      defaultValue: false,
       type: 'boolean',
     },
     size: {
       options: ['small', 'medium'],
+      defaultValue: 'medium',
       control: { type: 'inline-radio' },
     },
     required: {
+      defaultValue: false,
       type: 'boolean',
     },
     indeterminate: {
+      defaultValue: false,
       type: 'boolean',
     },
   },
-
-  render: function Render({ label, disabled, size, required, indeterminate }) {
+  parameters: {
+    layout: 'centered',
+    docs: {
+      toc: true,
+      source: {
+        code: null,
+      },
+      page: () => (
+        <>
+          <DocBlock.Title />
+          <DocBlock.Subtitle />
+          <DocBlock.Description />
+          <DocBlock.Source
+            code={`
+          <Checkbox
+  name={'custom'}
+  label={'Custom'}
+  checkboxProps={{ disabled: false, size: 'medium', indeterminate: false }}
+  labelProps={{ isRequired: false }}
+/>
+            `}
+          />
+          <DocBlock.Primary />
+          <DocBlock.Controls />
+          <DocBlock.Stories />
+        </>
+      ),
+    },
+  },
+  render: function Render({
+    label,
+    disabled,
+    size,
+    required,
+    indeterminate,
+    defaultChecked,
+  }) {
     const methods = useForm();
 
     return (
       <FormProvider {...methods}>
-        <Stack direction={'row'} columnGap={'20px'} justifyContent={'center'}>
-          <Checkbox
-            name="checkbox"
-            label="small"
-            checkboxProps={{ size: 'small' }}
-          />
-          <Checkbox
-            name="checkbox"
-            label="checked"
-            checkboxProps={{ size: 'small', defaultChecked: true }}
-          />
-          <Checkbox
-            name="checkbox"
-            label="disabled"
-            checkboxProps={{ size: 'small', disabled: true }}
-          />
-          <Checkbox
-            name="checkbox"
-            label="indeterminate"
-            checkboxProps={{ size: 'small', indeterminate: true }}
-          />
-        </Stack>
-        <Stack direction={'row'} columnGap={'20px'} justifyContent={'center'}>
-          <Checkbox
-            name="checkbox"
-            label="medium"
-            checkboxProps={{ size: 'medium' }}
-          />
-          <Checkbox
-            name="checkbox"
-            label="checked"
-            checkboxProps={{ size: 'medium', defaultChecked: true }}
-          />
-          <Checkbox
-            name="checkbox"
-            label="disabled"
-            checkboxProps={{ size: 'medium', disabled: true }}
-          />
-          <Checkbox
-            name="checkbox"
-            label="indeterminate"
-            checkboxProps={{ size: 'medium', indeterminate: true }}
-          />
-        </Stack>
-        <Stack alignItems={'center'}>
-          <Checkbox
-            name="custom"
-            label={label}
-            checkboxProps={{ disabled, size, indeterminate }}
-            labelProps={{ isRequired: required }}
-          />
-        </Stack>
+        <Checkbox
+          name="custom"
+          label={label}
+          checkboxProps={{ disabled, size, indeterminate, defaultChecked }}
+          labelProps={{ isRequired: required }}
+        />
       </FormProvider>
     );
   },
@@ -103,12 +94,48 @@ export default meta;
 
 type Story = StoryObj<CustomArgs>;
 
-export const Basic: Story = {
+export const Custom: Story = {
+  tags: ['!autodocs'],
   args: {
     label: 'Custom',
     disabled: false,
     size: 'medium',
     required: false,
     indeterminate: false,
+  },
+};
+
+export const Small: Story = {
+  args: {
+    label: 'Small',
+    size: 'small',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    label: 'Medium',
+    size: 'medium',
+  },
+};
+
+export const Checked: Story = {
+  args: {
+    label: 'Checked',
+    defaultChecked: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    label: 'Disabled',
+    disabled: true,
+  },
+};
+
+export const Indeterminate: Story = {
+  args: {
+    label: 'Indeterminate',
+    indeterminate: true,
   },
 };
