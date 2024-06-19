3aeabd80c36f9c698f936d5b7e55a5176314d827
Danila <officialdannisimo@gmail.com>
2024-06-19 04:13:54 +0400

    EVEREST-1088 update technology column in db list table

--- ui/apps/everest/src/pages/databases/DbClusterView.tsx
+++ ui/apps/everest/src/pages/databases/DbClusterView.tsx
@@ -22,7 +22,7 @@ import AddIcon from '@mui/icons-material/Add';
 import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
 import { Box, Button, MenuItem, Stack } from '@mui/material';
-import { Table } from '@percona/ui-lib';
+import { Table, humanizeDbType } from '@percona/ui-lib';
 import StatusField from 'components/status-field';
 import { useDbActions } from 'hooks/api/db-cluster/useDbActions';
 import { useNamespaces } from 'hooks/api/namespaces/useNamespaces';
@@ -41,11 +41,11 @@ import {
 } from './DbClusterView.utils';
 import { Messages } from './dbClusterView.messages';
 import { DbClusterTableElement } from './dbClusterView.types';
-import { DbTypeIconProvider } from './dbTypeIconProvider/DbTypeIconProvider';
 import { ExpandedRow } from './expandedRow/ExpandedRow';
 import { CustomConfirmDialog } from 'components/custom-confirm-dialog';
 import { LastBackup } from './lastBackup/LastBackup';
 import { useDbBackups } from 'hooks/api/backups/useBackups';
+import { dbEngineToDbType } from '@percona/utils';
 
 export const DbClusterView = () => {
   const [isNewClusterMode, setIsNewClusterMode] = useState(false);
@@ -127,7 +127,7 @@ export const DbClusterView = () => {
             alignItems="center"
             gap={1}
           >
-            <DbTypeIconProvider dbType={row.original?.dbType} />
+            {humanizeDbType(dbEngineToDbType(row.original?.dbType))}{' '}
             {row.original?.dbVersion}
           </Stack>
         ),
