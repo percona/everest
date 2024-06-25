7bcc7509546cd5b27e417062df4797beb4c79484
aElDi <varfst1995s@gmail.com>
2024-06-25 17:15:18 +0500

    [UI] EVEREST-996 AutoComplete Storybook Edit

--- ui/packages/ui-lib/src/form/inputs/auto-complete/auto-complete.stories.tsx
+++ ui/packages/ui-lib/src/form/inputs/auto-complete/auto-complete.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import * as DocBlock from '@storybook/blocks';
 import { FormProvider, useForm } from 'react-hook-form';
 import AutoCompleteInput from './auto-complete';
 
@@ -12,8 +13,44 @@ type CustomArgs = React.ComponentProps<typeof AutoCompleteInput> & {
 const meta = {
   title: 'AutoComplete',
   component: AutoCompleteInput,
+  tags: ['autodocs'],
   parameters: {
     layout: 'centered',
+    docs: {
+      source: null,
+    },
+    toc: true,
+    page: () => (
+      <>
+        <DocBlock.Title />
+        <DocBlock.Subtitle />
+        <DocBlock.Description />
+        <DocBlock.Source
+          code={`
+<AutoCompleteInput
+  textFieldProps={{
+    error: args.error,
+    size: args.size,
+    helperText: args.helperText,
+  }}
+  autoCompleteProps={{
+    sx: { width: args.width },
+    defaultValue: args.options[0],
+  }}
+  name={'autocomplete'}
+  label={args.label}
+  options={args.options}
+  disabled={args.disabled}
+  loading={args.loading}
+  isRequired={args.isRequired}
+/>
+            `}
+        />
+        <DocBlock.Primary />
+        <DocBlock.Controls />
+        <DocBlock.Stories />
+      </>
+    ),
   },
 
   argTypes: {
@@ -66,6 +103,7 @@ export default meta;
 type Story = StoryObj<CustomArgs>;
 
 export const Basic: Story = {
+  tags: ["!autodocs"],
   args: {
     label: 'Label',
     disabled: false,
@@ -76,5 +114,125 @@ export const Basic: Story = {
     options: ['First', 'Second', 'Third'],
     width: '200px',
     helperText: 'Select one of the options',
+  },
+};
+
+export const Small: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: `\`\`\`ts
+<AutoCompleteInput textFieldProps={{size: 'small'}} />
+        \`\`\``
+      }
+    }
+  },
+  args: {
+    label: 'Small',
+    size: 'small',
+    options: ['First', 'Second', 'Third'],
+    width: '200px',
+  },
+};
+
+export const Medium: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: `\`\`\`ts
+<AutoCompleteInput textFieldProps={{size: 'medium'}} />
+        \`\`\``
+      }
+    }
+  },
+  args: {
+    label: 'Medium',
+    size: 'medium',
+    options: ['First', 'Second', 'Third'],
+    width: '200px',
+  },
+};
+
+export const Required: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: `\`\`\`ts
+<AutoCompleteInput isRequired={true} />
+        \`\`\``
+      }
+    }
+  },
+  args: {
+    label: 'Required',
+    size: 'small',
+    isRequired: true,
+    options: ['First', 'Second', 'Third'],
+    width: '200px',
+  },
+};
+
+export const Disabled: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: `\`\`\`ts
+<AutoCompleteInput disabled={true} />
+        \`\`\``
+      }
+    }
+  },
+  args: {
+    label: 'Disabled',
+    size: 'small',
+    disabled: true,
+    options: ['First', 'Second', 'Third'],
+    width: '200px',
+  },
+};
+
+
+
+export const Error: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: `\`\`\`ts
+<AutoCompleteInput textFieldProps={
+  {
+    error: true,
+    helperText: 'Error helper text'
+  }
+} />
+        \`\`\``
+      }
+    }
+  },
+  args: {
+    label: 'Error with helper',
+    size: 'small',
+    error: true,
+    helperText: "Error helper text",
+    options: ['First', 'Second', 'Third'],
+    width: '200px',
+  },
+};
+
+export const Loading: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: `\`\`\`ts
+<AutoCompleteInput loading={true} />
+        \`\`\``
+      }
+    }
+  },
+  args: {
+    label: 'Loading',
+    loading: true,
+    size: 'small',
+    options: ['First', 'Second', 'Third'],
+    width: '200px',
   },
 };
