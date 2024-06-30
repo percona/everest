1c22a352fab0fddddb3ef395f9671493711c2197
X6uddy <sashavolkov_03@mail.ru>
2024-06-30 16:39:14 +0400

    EVEREST-1044-storybook-typography creating a story in storybook for the Typography component

--- ui/packages/ui-lib/src/stories/Typography/Typography.data.ts
+++ ui/packages/ui-lib/src/stories/Typography/Typography.data.ts
@@ -2,167 +2,82 @@ import { TypographyPropsVariantOverrides } from '@mui/material';
 import { Variant } from '@mui/material/styles/createTypography';
 import { OverridableStringUnion } from '@mui/types';
 
-interface Typography {
-  [key: string]: Array<{
-    title: string;
-    variant: OverridableStringUnion<
-      Variant | 'inherit',
-      TypographyPropsVariantOverrides
-    >;
-    text: string;
-    style?: {
-      fontFamily?: string;
-      fontSize?: string;
-      fontWeight?: string;
-      lineHeight?: string;
-      color?: string;
-      textDecoration?: string;
-    };
-  }>;
-}
-export const TYPOGRAPHY: Typography = {
-  'SM/MD/LG/XL (600+)': [
+type Typography = Array<{
+  variant: OverridableStringUnion<
+    Variant | 'inherit',
+    TypographyPropsVariantOverrides
+  >;
+  text: string;
+  style?: {
+    fontFamily?: string;
+    fontSize?: string;
+    fontWeight?: string;
+    lineHeight?: string;
+    color?: string;
+    textDecoration?: string;
+  };
+}>[];
+export const TYPOGRAPHY: Typography = [
+  [
     {
-      title: 'h1',
       variant: 'h1',
       text: 'Disregard and contempt',
-      style: {
-        fontSize: '48px',
-      },
     },
     {
-      title: 'h2',
       variant: 'h2',
       text: 'Whereas disregard and contempt for human rights',
-      style: {
-        fontSize: '40px',
-      },
     },
     {
-      title: 'h3',
       variant: 'h3',
       text: 'Whereas disregard and contempt for human rights have resulted',
-      style: {
-        fontSize: '33px',
-      },
     },
     {
-      title: 'h4',
       variant: 'h4',
       text: 'No one shall be held in slavery or servitude; slavery and the slave trade shall be prohibited',
-      style: {
-        fontSize: '28px',
-      },
     },
     {
-      title: 'h5',
       variant: 'h5',
       text: 'No one shall be held in slavery or servitude; slavery and the slave trade shall be prohibited in all their forms',
-      style: {
-        fontSize: '23px',
-      },
     },
     {
-      title: 'h6',
       variant: 'h6',
       text: 'Everyone has the right to an effective remedy by the competent national tribunals for acts violating the fundamental rights granted him by the constitution or by law',
-      style: {
-        fontSize: '19px',
-      },
     },
   ],
-  'XS (0-599)': [
+  [
     {
-      title: 'h1',
-      variant: 'h1',
-      text: 'Disregard and contempt',
-      style: {
-        fontSize: '32px',
-      },
-    },
-    {
-      title: 'h2',
-      variant: 'h2',
-      text: 'Whereas disregard and contempt for human rights',
-      style: {
-        fontSize: '29px',
-      },
-    },
-    {
-      title: 'h3',
-      variant: 'h3',
-      text: 'Whereas disregard and contempt for human rights have resulted',
-      style: {
-        fontSize: '26px',
-      },
-    },
-    {
-      title: 'h4',
-      variant: 'h4',
-      text: 'No one shall be held in slavery or servitude; slavery and the slave trade shall be prohibited',
-      style: {
-        fontSize: '23px',
-      },
-    },
-    {
-      title: 'h5',
-      variant: 'h5',
-      text: 'No one shall be held in slavery or servitude; slavery and the slave trade shall be prohibited in all their forms',
-      style: {
-        fontSize: '20px',
-      },
-    },
-    {
-      title: 'h6',
-      variant: 'h6',
-      text: 'Everyone has the right to an effective remedy by the competent national tribunals for acts violating the fundamental rights granted him by the constitution or by law',
-      style: {
-        fontSize: '18px',
-      },
-    },
-  ],
-  Titling: [
-    {
-      title: 'subHead 1',
       variant: 'subHead1',
       text: 'Everyone has the right to an effective remedy by the competent national tribunals for acts violating the fundamental rights granted him by the constitution or by law.',
     },
     {
-      title: 'subHead 2',
       variant: 'subHead2',
       text: 'No one shall be subjected to arbitrary arrest, detention or exile. Everyone is entitled in full equality to a fair and public hearing by an independent and impartial tribunal, in the determination of his rights and obligations and of any criminal charge against him.',
     },
     {
-      title: 'overline',
       variant: 'overline',
       text: 'Everyone has the right to an effective remedy',
     },
     {
-      title: 'section heading',
       variant: 'sectionHeading',
       text: 'Everyone is entitled in full equality to a fair and public hearing.',
     },
   ],
-  Body: [
+  [
     {
-      title: 'body1',
       variant: 'body1',
       text: 'No one shall be subjected to arbitrary arrest, detention or exile. Everyone is entitled in full equality to a fair and public hearing by an independent and impartial tribunal, in the determination of his rights and obligations and of any criminal charge against him. No one shall be subjected to arbitrary interference with his privacy, family, home or correspondence, nor to attacks upon his honour and reputation. Everyone has the right to the protection of the law against such interference or attacks.',
     },
     {
-      title: 'body2',
       variant: 'body2',
       text: 'No one shall be subjected to arbitrary arrest, detention or exile. Everyone is entitled in full equality to a fair and public hearing by an independent and impartial tribunal, in the determination of his rights and obligations and of any criminal charge against him. No one shall be subjected to arbitrary interference with his privacy, family, home or correspondence, nor to attacks upon his honour and reputation. Everyone has the right to the protection of the law against such interference or attacks.',
     },
     {
-      title: 'caption',
       variant: 'caption',
       text: 'No one shall be subjected to arbitrary arrest, detention or exile. Everyone is entitled in full equality to a fair and public hearing by an independent and impartial tribunal, in the determination of his rights and obligations and of any criminal charge against him. No one shall be subjected to arbitrary interference with his privacy, family, home or correspondence, nor to attacks upon his honour and reputation. Everyone has the right to the protection of the law against such interference or attacks.',
     },
   ],
-  Actions: [
+  [
     {
-      title: 'Button Large',
       variant: 'button',
       text: 'Whereas recognition',
       style: {
@@ -174,7 +89,6 @@ export const TYPOGRAPHY: Typography = {
       },
     },
     {
-      title: 'Button Medium',
       variant: 'button',
       text: 'Whereas recognition',
       style: {
@@ -186,7 +100,6 @@ export const TYPOGRAPHY: Typography = {
       },
     },
     {
-      title: 'Button small',
       variant: 'button',
       text: 'Whereas recognition',
       style: {
@@ -198,7 +111,6 @@ export const TYPOGRAPHY: Typography = {
       },
     },
     {
-      title: 'Menu Text',
       variant: 'menuText',
       text: 'Whereas recognition',
       style: {
@@ -210,7 +122,6 @@ export const TYPOGRAPHY: Typography = {
       },
     },
     {
-      title: 'Body 1 link',
       variant: 'body1',
       text: 'Whereas recognition',
       style: {
@@ -223,7 +134,6 @@ export const TYPOGRAPHY: Typography = {
       },
     },
     {
-      title: 'Body 2 link',
       variant: 'body2',
       text: 'Whereas recognition',
       style: {
@@ -236,21 +146,18 @@ export const TYPOGRAPHY: Typography = {
       },
     },
   ],
-  Inputs: [
+  [
     {
-      title: 'Input text',
       variant: 'inputText',
       text: 'Whereas recognition',
     },
     {
-      title: 'Input label',
       variant: 'inputLabel',
       text: 'Whereas recognition',
     },
     {
-      title: 'Helper text',
       variant: 'helperText',
       text: 'Whereas recognition',
     },
   ],
-};
+];
--- ui/packages/ui-lib/src/stories/Typography/Typography.stories.tsx
+++ ui/packages/ui-lib/src/stories/Typography/Typography.stories.tsx
@@ -1,88 +1,348 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import {
-  Box,
-  Divider,
-  Stack,
-  Typography,
-  TypographyProps,
-} from '@mui/material';
+import * as DocBlock from '@storybook/blocks';
 import { TYPOGRAPHY } from './Typography.data';
-
-const itemTitleStyles = {
-  fontSize: '20px',
-  fontWeight: '700',
-  color: '#2C323E66',
-  lineHeight: '20px',
-  textAlign: 'right',
-  align: 'right',
-};
-const boxItemStyles = {
-  width: '1040px',
-  boxSizing: 'border-box',
-  display: 'flex',
-  alignItems: 'baseline',
-  gap: '80px',
-  padding: '24px 40px 24px 0',
-};
-const mainBoxStyles = {
-  border: '1px solid #2C323E40',
-  boxSizing: 'border-box',
-  width: '1120px',
-  padding: '40px',
-  borderRadius: '16px',
-};
-const mainTitleStyles = {
-  fontWeight: '700',
-  fontSize: '20px',
-  marginBottom: '40px',
-  color: '#2C323E',
-};
-
-const textBoxStyles = {
-  boxSizing: 'border-box',
-  width: '156px',
-  flexShrink: '0',
-};
+import { Typography, TypographyProps } from '@mui/material';
 
 const meta = {
   title: 'Typography',
-  component: Typography,
+  tags: ['autodocs'],
   parameters: {
     layout: 'centered',
+    docs: {
+      source: {
+        code: null,
+      },
+      page: () => (
+        <>
+          <DocBlock.Title />
+          <DocBlock.Subtitle />
+          <DocBlock.Description />
+          <DocBlock.Stories />
+        </>
+      ),
+    },
   },
-  render: function Render() {
-    return (
-      <Box>
-        {Object.keys(TYPOGRAPHY).map((key) => (
-          <Box key={key} my="80px">
-            <Typography sx={mainTitleStyles}>{key}</Typography>
-            <Box sx={mainBoxStyles}>
-              <Stack divider={<Divider orientation="horizontal" flexItem />}>
-                {TYPOGRAPHY[key].map((item, i) => (
-                  <Box sx={boxItemStyles} key={i}>
-                    <Box sx={textBoxStyles}>
-                      <Typography sx={itemTitleStyles}>
-                        {item.title.toUpperCase()}
-                      </Typography>
-                    </Box>
-                    <Typography
-                      variant={item.variant}
-                      style={{ ...item.style }}
-                    >
-                      {item.text}
-                    </Typography>
-                  </Box>
-                ))}
-              </Stack>
-            </Box>
-          </Box>
-        ))}
-      </Box>
-    );
-  },
-} satisfies Meta<TypographyProps>;
+} satisfies Meta;
 
 export default meta;
 type Story = StoryObj<TypographyProps>;
 
-export const MainScale: Story = {};
+export const H1: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="h1">Disregard and contempt</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[0][0].variant}>
+          {TYPOGRAPHY[0][0].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const H2: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="h2">Whereas disregard and contempt for human rights</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[0][1].variant}>
+          {TYPOGRAPHY[0][1].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const H3: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="h3">Whereas disregard and contempt for human rights have resulted</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[0][2].variant}>
+          {TYPOGRAPHY[0][2].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const H4: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="h4">No one shall be held in slavery or servitude; slavery and the slave trade shall be prohibited</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[0][3].variant}>
+          {TYPOGRAPHY[0][3].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const H5: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="h5">No one shall be held in slavery or servitude; slavery and the slave trade shall be prohibited in all their forms</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[0][4].variant}>
+          {TYPOGRAPHY[0][4].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const H6: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="h6">Everyone has the right to an effective remedy by the competent national tribunals for acts violating the fundamental rights granted him by the constitution or by law</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[0][5].variant}>
+          {TYPOGRAPHY[0][5].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const Subhead1: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="subHead1">Everyone has the right to an effective remedy by the competent national tribunals for acts violating the fundamental rights granted him by the constitution or by law.</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[1][0].variant}>
+          {TYPOGRAPHY[1][0].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const Subhead2: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="subHead2">No one shall be subjected to arbitrary arrest, detention or exile. Everyone is entitled in full equality to a fair and public hearing by an independent and impartial tribunal, in the determination of his rights and obligations and of any criminal charge against him.</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[1][1].variant}>
+          {TYPOGRAPHY[1][1].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const Overline: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="overline">Everyone has the right to an effective remedy</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[1][2].variant}>
+          {TYPOGRAPHY[1][2].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const SectionHeading: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="sectionHeading">Everyone is entitled in full equality to a fair and public hearing.</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[1][3].variant}>
+          {TYPOGRAPHY[1][3].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const Body1: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="body1">No one shall be subjected to arbitrary arrest, detention or exile. Everyone is entitled in full equality to a fair and public hearing by an independent and impartial tribunal, in the determination of his rights and obligations and of any criminal charge against him. No one shall be subjected to arbitrary interference with his privacy, family, home or correspondence, nor to attacks upon his honour and reputation. Everyone has the right to the protection of the law against such interference or attacks.</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[2][0].variant}>
+          {TYPOGRAPHY[2][0].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const Body2: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="body2">No one shall be subjected to arbitrary arrest, detention or exile. Everyone is entitled in full equality to a fair and public hearing by an independent and impartial tribunal, in the determination of his rights and obligations and of any criminal charge against him. No one shall be subjected to arbitrary interference with his privacy, family, home or correspondence, nor to attacks upon his honour and reputation. Everyone has the right to the protection of the law against such interference or attacks.</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[2][1].variant}>
+          {TYPOGRAPHY[2][1].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const Caption: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="caption">No one shall be subjected to arbitrary arrest, detention or exile. Everyone is entitled in full equality to a fair and public hearing by an independent and impartial tribunal, in the determination of his rights and obligations and of any criminal charge against him. No one shall be subjected to arbitrary interference with his privacy, family, home or correspondence, nor to attacks upon his honour and reputation. Everyone has the right to the protection of the law against such interference or attacks.</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[2][2].variant}>
+          {TYPOGRAPHY[2][2].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const InputText: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="inputText">Whereas recognition</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[4][0].variant}>
+          {TYPOGRAPHY[4][0].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const InputLabel: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="inputLabel">Whereas recognition</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[4][1].variant}>
+          {TYPOGRAPHY[4][1].text}
+        </Typography>
+      </>
+    );
+  },
+};
+
+export const HelperText: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Typography variant="helperText">Whereas recognition</Typography>',
+      },
+    },
+  },
+
+  render: function Render() {
+    return (
+      <>
+        <Typography variant={TYPOGRAPHY[4][2].variant}>
+          {TYPOGRAPHY[4][2].text}
+        </Typography>
+      </>
+    );
+  },
+};
