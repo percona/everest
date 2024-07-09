385a439a30b3f2384161f91a13df21efccbab503
Fábio Silva <ffjs1993@gmail.com>
2024-07-09 15:30:52 +0100

    feat: use GiB as disk unit (#478)
    * feat: use GiB as disk unit
    
    * fix: e2e
    
    * fix: unit test

--- ui/apps/everest/src/utils/k8ResourceParser/index.ts
+++ ui/apps/everest/src/utils/k8ResourceParser/index.ts
@@ -1,16 +1,30 @@
-const memoryMultipliers = {
-  k: 10 ** -6,
-  M: 10 ** -3,
-  G: 1,
-  T: 1000,
-  P: 1000 ** 2,
-  E: 1000 ** 3,
-  Ki: 1024 * 10 ** -9,
-  Mi: 1024 ** 2 * 10 ** -9,
-  Gi: 1024 ** 3 * 10 ** -9,
-  Ti: 1024 ** 4 * 10 ** -9,
-  Pi: 1024 ** 5 * 10 ** -9,
-  Ei: 1024 ** 6 * 10 ** -9,
+type kubernetesUnit =
+  | 'k'
+  | 'M'
+  | 'G'
+  | 'T'
+  | 'P'
+  | 'E'
+  | 'Ki'
+  | 'Mi'
+  | 'Gi'
+  | 'Ti'
+  | 'Pi'
+  | 'Ei';
+
+const memoryMultipliers: Record<kubernetesUnit, number> = {
+  k: 10 ** 3,
+  M: 10 ** 6,
+  G: 10 ** 9,
+  T: 10 ** 12,
+  P: 10 ** 15,
+  E: 10 ** 18,
+  Ki: 1024,
+  Mi: 1024 ** 2,
+  Gi: 1024 ** 3,
+  Ti: 1024 ** 4,
+  Pi: 1024 ** 5,
+  Ei: 1024 ** 6,
 };
 
 export const cpuParser = (input: string) => {
@@ -24,12 +38,17 @@ export const cpuParser = (input: string) => {
   return parseFloat(input);
 };
 
-export const memoryParser = (input: string) => {
+export const memoryParser = (input: string, targetUnit?: kubernetesUnit) => {
   const unitMatch = input.match(/^([0-9]+)([A-Za-z]{1,2})$/);
 
-  if (unitMatch) {
+  if (targetUnit && unitMatch) {
+    const value = parseInt(unitMatch[1], 10);
     // @ts-ignore
-    return parseInt(unitMatch[1], 10) * memoryMultipliers[unitMatch[2]];
+    const sourceUnit: kubernetesUnit = unitMatch[2];
+
+    const valueBytes = value * memoryMultipliers[sourceUnit];
+
+    return valueBytes / memoryMultipliers[targetUnit];
   }
 
   return parseInt(input, 10);
