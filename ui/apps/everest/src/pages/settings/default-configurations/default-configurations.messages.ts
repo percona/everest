9a76c838e1c1da1e5bdc05cbbd92539e0c9f6803
dianabirs <dianabirs@users.noreply.github.com>
2024-07-25 08:50:35 +0000

    chore: lint/format

--- ui/apps/everest/src/pages/settings/default-configurations/default-configurations.messages.ts
+++ ui/apps/everest/src/pages/settings/default-configurations/default-configurations.messages.ts
@@ -12,7 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 
-import {  SOURCE_RANGE_MESSAGE } from 'consts';
+import { SOURCE_RANGE_MESSAGE } from 'consts';
 
 // limitations under the License.
 export const Messages = {
