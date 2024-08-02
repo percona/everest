e88cefb688c1aa96f72813cd465887eb45552235
Diana Birsan <diana.birsan@3pillarglobal.com>
2024-08-02 16:28:39 +0300

    [UI] EVEREST-1286 Backup name should be disabled for editing in backup schedules #567

--- ui/apps/everest/src/components/schedule-form-dialog/schedule-form/schedule-form.types.ts
+++ ui/apps/everest/src/components/schedule-form-dialog/schedule-form/schedule-form.types.ts
@@ -15,6 +15,7 @@ export type ScheduleFormProps = {
   schedules: Schedule[];
   showTypeRadio: boolean;
   hideRetentionCopies?: boolean;
+  disableNameEdit?: boolean;
 };
 
 export const ScheduleFormFields = { ...ScheduleForm, ...TimeSelectionFields };
