05a0bc1bdfed2e72f2c5840833631342269253ee
Mayank Shah <mayank.shah@percona.com>
2024-08-13 12:37:14 +0530

    Add tests
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- pkg/common/version_test.go
+++ pkg/common/version_test.go
@@ -32,4 +32,8 @@ func TestCheckConstraints(t *testing.T) {
 	assert.False(t, CheckConstraint("1.0.0", "< 1.0.0"))
 	assert.True(t, CheckConstraint("1.1.0", "~> 1.1.0"))
 	assert.False(t, CheckConstraint("1.2.0", "~> 1.1.0"))
+	assert.True(t, CheckConstraint("1.2.0-rc1", "~> 1.2.0"))
+	assert.True(t, CheckConstraint("1.2.1-rc1", "~> 1.2.0"))
+	assert.False(t, CheckConstraint("1.3.1-rc1", "~> 1.2.0"))
+	assert.True(t, CheckConstraint("1.3.1-rc1", "> 1.2.0"))
 }
