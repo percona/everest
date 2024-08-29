03e9b3d01a5a25f137142b68e26f6101905c1fb8
Oksana Grishchenko <oksana.grishchenko@percona.com>
2024-08-29 23:54:31 +0200

    validation & tests

--- api/validation.go
+++ api/validation.go
@@ -58,8 +58,10 @@ const (
 	pgReposLimit        = 3
 	// We are diverging from the RFC1035 spec in regards to the length of the
 	// name because the PXC operator limits the name of the cluster to 22.
-	maxNameLength      = 22
-	timeoutS3AccessSec = 2
+	maxNameLength       = 22
+	timeoutS3AccessSec  = 2
+	minShardsNum        = 1
+	minConfigServersNum = 3
 )
 
 var (
@@ -67,47 +69,54 @@ var (
 	minCPUQuantity     = resource.MustParse("600m") //nolint:gochecknoglobals
 	minMemQuantity     = resource.MustParse("512M") //nolint:gochecknoglobals
 
-	errDBCEmptyMetadata              = errors.New("databaseCluster's Metadata should not be empty")
-	errDBCNameEmpty                  = errors.New("databaseCluster's metadata.name should not be empty")
-	errDBCNamespaceEmpty             = errors.New("databaseCluster's metadata.namespace should not be empty")
-	errDBCNameWrongFormat            = errors.New("databaseCluster's metadata.name should be a string")
-	errDBCNamespaceWrongFormat       = errors.New("databaseCluster's metadata.namespace should be a string")
-	errNotEnoughMemory               = fmt.Errorf("memory limits should be above %s", minMemQuantity.String())
-	errInt64NotSupported             = errors.New("specifying resources using int64 data type is not supported. Please use string format for that")
-	errNotEnoughCPU                  = fmt.Errorf("CPU limits should be above %s", minCPUQuantity.String())
-	errNotEnoughDiskSize             = fmt.Errorf("storage size should be above %s", minStorageQuantity.String())
-	errUnsupportedPXCProxy           = errors.New("you can use either HAProxy or Proxy SQL for PXC clusters")
-	errUnsupportedPGProxy            = errors.New("you can use only PGBouncer as a proxy type for Postgres clusters")
-	errUnsupportedPSMDBProxy         = errors.New("you can use only Mongos as a proxy type for MongoDB clusters")
-	errNoSchedules                   = errors.New("please specify at least one backup schedule")
-	errNoNameInSchedule              = errors.New("'name' field for the backup schedules cannot be empty")
-	errScheduleNoBackupStorageName   = errors.New("'backupStorageName' field cannot be empty when schedule is enabled")
-	errPitrNoBackupStorageName       = errors.New("'backupStorageName' field cannot be empty when pitr is enabled")
-	errNoResourceDefined             = errors.New("please specify resource limits for the cluster")
-	errPitrUploadInterval            = errors.New("'uploadIntervalSec' should be more than 0")
-	errPXCPitrS3Only                 = errors.New("point-in-time recovery only supported for s3 compatible storages")
-	errPSMDBMultipleStorages         = errors.New("can't use more than one backup storage for PSMDB clusters")
-	errPSMDBViolateActiveStorage     = errors.New("can't change the active storage for PSMDB clusters")
-	errDataSourceConfig              = errors.New("either DBClusterBackupName or BackupSource must be specified in the DataSource field")
-	errDataSourceNoPitrDateSpecified = errors.New("pitr Date must be specified for type Date")
-	errDataSourceWrongDateFormat     = errors.New("failed to parse .Spec.DataSource.Pitr.Date as 2006-01-02T15:04:05Z")
-	errDataSourceNoBackupStorageName = errors.New("'backupStorageName' should be specified in .Spec.DataSource.BackupSource")
-	errDataSourceNoPath              = errors.New("'path' should be specified in .Spec.DataSource.BackupSource")
-	errIncorrectDataSourceStruct     = errors.New("incorrect data source struct")
-	errUnsupportedPitrType           = errors.New("the given point-in-time recovery type is not supported")
-	errTooManyPGStorages             = fmt.Errorf("only %d different storages are allowed in a PostgreSQL cluster", pgReposLimit)
-	errNoMetadata                    = fmt.Errorf("no metadata provided")
-	errInvalidResourceVersion        = fmt.Errorf("invalid 'resourceVersion' value")
-	errInvalidBucketName             = fmt.Errorf("invalid bucketName")
-	errInvalidVersion                = errors.New("invalid database engine version provided")
-	errDBEngineMajorVersionUpgrade   = errors.New("database engine cannot be upgraded to a major version")
-	errDBEngineDowngrade             = errors.New("database engine version cannot be downgraded")
-	errDuplicatedSchedules           = errors.New("duplicated backup schedules are not allowed")
-	errDuplicatedStoragePG           = errors.New("postgres clusters can't use the same storage for the different schedules")
-	errStorageChangePG               = errors.New("the existing postgres schedules can't change their storage")
-	errDuplicatedBackupStorage       = errors.New("backup storages with the same url, bucket and url are not allowed")
-	errEditBackupStorageInUse        = errors.New("can't edit bucket or region of the backup storage in use")
-	errInsufficientPermissions       = errors.New("insufficient permissions for performing the operation")
+	errDBCEmptyMetadata                     = errors.New("databaseCluster's Metadata should not be empty")
+	errDBCNameEmpty                         = errors.New("databaseCluster's metadata.name should not be empty")
+	errDBCNamespaceEmpty                    = errors.New("databaseCluster's metadata.namespace should not be empty")
+	errDBCNameWrongFormat                   = errors.New("databaseCluster's metadata.name should be a string")
+	errDBCNamespaceWrongFormat              = errors.New("databaseCluster's metadata.namespace should be a string")
+	errNotEnoughMemory                      = fmt.Errorf("memory limits should be above %s", minMemQuantity.String())
+	errInt64NotSupported                    = errors.New("specifying resources using int64 data type is not supported. Please use string format for that")
+	errNotEnoughCPU                         = fmt.Errorf("CPU limits should be above %s", minCPUQuantity.String())
+	errNotEnoughDiskSize                    = fmt.Errorf("storage size should be above %s", minStorageQuantity.String())
+	errUnsupportedPXCProxy                  = errors.New("you can use either HAProxy or Proxy SQL for PXC clusters")
+	errUnsupportedPGProxy                   = errors.New("you can use only PGBouncer as a proxy type for Postgres clusters")
+	errUnsupportedPSMDBProxy                = errors.New("you can use only Mongos as a proxy type for MongoDB clusters")
+	errNoSchedules                          = errors.New("please specify at least one backup schedule")
+	errNoNameInSchedule                     = errors.New("'name' field for the backup schedules cannot be empty")
+	errScheduleNoBackupStorageName          = errors.New("'backupStorageName' field cannot be empty when schedule is enabled")
+	errPitrNoBackupStorageName              = errors.New("'backupStorageName' field cannot be empty when pitr is enabled")
+	errNoResourceDefined                    = errors.New("please specify resource limits for the cluster")
+	errPitrUploadInterval                   = errors.New("'uploadIntervalSec' should be more than 0")
+	errPXCPitrS3Only                        = errors.New("point-in-time recovery only supported for s3 compatible storages")
+	errPSMDBMultipleStorages                = errors.New("can't use more than one backup storage for PSMDB clusters")
+	errPSMDBViolateActiveStorage            = errors.New("can't change the active storage for PSMDB clusters")
+	errDataSourceConfig                     = errors.New("either DBClusterBackupName or BackupSource must be specified in the DataSource field")
+	errDataSourceNoPitrDateSpecified        = errors.New("pitr Date must be specified for type Date")
+	errDataSourceWrongDateFormat            = errors.New("failed to parse .Spec.DataSource.Pitr.Date as 2006-01-02T15:04:05Z")
+	errDataSourceNoBackupStorageName        = errors.New("'backupStorageName' should be specified in .Spec.DataSource.BackupSource")
+	errDataSourceNoPath                     = errors.New("'path' should be specified in .Spec.DataSource.BackupSource")
+	errIncorrectDataSourceStruct            = errors.New("incorrect data source struct")
+	errUnsupportedPitrType                  = errors.New("the given point-in-time recovery type is not supported")
+	errTooManyPGStorages                    = fmt.Errorf("only %d different storages are allowed in a PostgreSQL cluster", pgReposLimit)
+	errNoMetadata                           = fmt.Errorf("no metadata provided")
+	errInvalidResourceVersion               = fmt.Errorf("invalid 'resourceVersion' value")
+	errInvalidBucketName                    = fmt.Errorf("invalid bucketName")
+	errInvalidVersion                       = errors.New("invalid database engine version provided")
+	errDBEngineMajorVersionUpgrade          = errors.New("database engine cannot be upgraded to a major version")
+	errDBEngineDowngrade                    = errors.New("database engine version cannot be downgraded")
+	errDuplicatedSchedules                  = errors.New("duplicated backup schedules are not allowed")
+	errDuplicatedStoragePG                  = errors.New("postgres clusters can't use the same storage for the different schedules")
+	errStorageChangePG                      = errors.New("the existing postgres schedules can't change their storage")
+	errDuplicatedBackupStorage              = errors.New("backup storages with the same url, bucket and url are not allowed")
+	errEditBackupStorageInUse               = errors.New("can't edit bucket or region of the backup storage in use")
+	errInsufficientPermissions              = errors.New("insufficient permissions for performing the operation")
+	errShardingIsNotSupported               = errors.New("sharding is not supported")
+	errInsufficientShardsNumber             = errors.New("shards number should be greater than 0")
+	errInsufficientConfigServersNumber      = errors.New("sharding: minimum config servers number is 3")
+	errEvenServersNumber                    = errors.New("sharding: config servers number should be odd")
+	errDisableShardingNotSupported          = errors.New("sharding: disable sharding is not supported")
+	errChangeShardsNumberNotSupported       = errors.New("sharding: change shards number is not supported")
+	errChangeConfigServerNumberNotSupported = errors.New("sharding: change config server number is not supported")
 
 	//nolint:gochecknoglobals
 	operatorEngine = map[everestv1alpha1.EngineType]string{
@@ -665,7 +674,29 @@ func (e *EverestServer) validateDatabaseClusterCR(
 			return err
 		}
 	}
+	if err := validateSharding(*databaseCluster); err != nil {
+		return err
+	}
 	return validateResourceLimits(databaseCluster)
+}
+
+func validateSharding(dbc DatabaseCluster) error {
+	if dbc.Spec.Sharding == nil || !dbc.Spec.Sharding.Enabled {
+		return nil
+	}
+	if dbc.Spec.Engine.Type != Psmdb {
+		return errShardingIsNotSupported
+	}
+	if dbc.Spec.Sharding.Shards < minShardsNum {
+		return errInsufficientShardsNumber
+	}
+	if dbc.Spec.Sharding.ConfigServer.Replicas < minConfigServersNum {
+		return errInsufficientConfigServersNumber
+	}
+	if dbc.Spec.Sharding.ConfigServer.Replicas%2 == 0 {
+		return errEvenServersNumber
+	}
+	return nil
 }
 
 func (e *EverestServer) validatePGSchedulesRestrictions(ctx context.Context, newDbc DatabaseCluster) error {
@@ -1118,6 +1149,10 @@ func (e *EverestServer) validateDatabaseClusterOnUpdate(
 		return fmt.Errorf("cannot scale down %d node cluster to 1. The operation is not supported", oldDB.Spec.Engine.Replicas)
 	}
 
+	if err := validateShardingOnUpdate(dbc, oldDB); err != nil {
+		return err
+	}
+
 	user, err := rbac.GetUser(c)
 	if err != nil {
 		return errors.Join(err, errors.New("cannot get user from request context"))
@@ -1126,6 +1161,22 @@ func (e *EverestServer) validateDatabaseClusterOnUpdate(
 		return err
 	}
 	return nil
+}
+
+func validateShardingOnUpdate(dbc *DatabaseCluster, oldDB *everestv1alpha1.DatabaseCluster) error {
+	if oldDB.Spec.Sharding == nil || !oldDB.Spec.Sharding.Enabled {
+		return nil
+	}
+	if dbc.Spec.Sharding == nil || !dbc.Spec.Sharding.Enabled {
+		return errDisableShardingNotSupported
+	}
+	if dbc.Spec.Sharding.Shards != oldDB.Spec.Sharding.Shards {
+		return errChangeShardsNumberNotSupported
+	}
+	if dbc.Spec.Sharding.ConfigServer.Replicas != oldDB.Spec.Sharding.ConfigServer.Replicas {
+		return errChangeConfigServerNumberNotSupported
+	}
+	return validateSharding(*dbc)
 }
 
 func (e *EverestServer) validateDatabaseClusterBackup(ctx context.Context, namespace string, backup *DatabaseClusterBackup) error {
