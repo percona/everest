a96a4d593d5de635e0fff6a456eb6d0bf6c8603f
yurkovychv <vasyl.yurkovych@percona.com>
2024-09-17 12:18:03 +0300

    EVEREST-1475 use type generation for API tests

---
+++ api-tests/support/types/database-engines.ts
@@ -0,0 +1,4 @@
+import { paths } from '@generatedTypes';
+
+export type DatabaseEnginesResponse = paths['/namespaces/{namespace}/database-engines']['get']['responses']['200']['content']['application/json'];
+export type DatabaseEngineResponse = paths['/namespaces/{namespace}/database-engines/{name}']['get']['responses']['200']['content']['application/json'];
--- api-tests/tests/database-engines.spec.ts
+++ api-tests/tests/database-engines.spec.ts
@@ -12,52 +12,47 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { test, expect } from '@fixtures'
-import {checkError, testsNs} from "@tests/tests/helpers";
+import { test, expect } from '@fixtures';
+import { checkError, testsNs } from '@tests/tests/helpers';
+import { DatabaseEngineResponse, DatabaseEnginesResponse } from '@support/types/database-engines';
 
 test('check operators are installed', async ({ request }) => {
-  const enginesList = await request.get(`/v1/namespaces/${testsNs}/database-engines`)
+  const enginesList = await request.get(`/v1/namespaces/${testsNs}/database-engines`);
 
-  await checkError(enginesList)
+  await checkError(enginesList);
+  const engines = (await enginesList.json() as DatabaseEnginesResponse).items;
 
-  const engines = (await enginesList.json()).items
-
-  engines.forEach((engine) => {
-    if (engine.spec.type === 'pxc') {
-      expect(engine.status?.status).toBe('installed')
-    }
-
-    if (engine.spec.type === 'psmdb') {
-      expect(engine.status?.status).toBe('installed')
-    }
-  })
-})
+  engines
+    .filter((engine) => engine.spec.type !== 'postgresql')
+    .forEach((engine) => {
+      expect(engine.status?.status).toBe('installed');
+    });
+});
 
 test('get/edit database engine versions', async ({ request }) => {
+  let engineResponse = await request.get(`/v1/namespaces/${testsNs}/database-engines/percona-server-mongodb-operator`);
 
-  let engineResponse = await request.get(`/v1/namespaces/${testsNs}/database-engines/percona-server-mongodb-operator`)
+  await checkError(engineResponse);
 
-  await checkError(engineResponse)
+  const engineData: DatabaseEngineResponse = await engineResponse.json();
+  const availableVersions = engineData.status.availableVersions;
 
-  const engineData = await engineResponse.json()
-  const availableVersions = engineData.status.availableVersions
+  expect(availableVersions.engine['7.0.12-7'].imageHash).toBe('7f00e19878bd143119772cd5468f1f0f9857dfcd2ae2f814d52ef3fa7cff6899');
+  expect(availableVersions.backup['2.5.0'].status).toBe('recommended');
 
-  expect(availableVersions.engine['7.0.12-7'].imageHash).toBe('7f00e19878bd143119772cd5468f1f0f9857dfcd2ae2f814d52ef3fa7cff6899')
-  expect(availableVersions.backup['2.5.0'].status).toBe('recommended')
+  const allowedVersions = ['6.0.5-4', '6.0.4-3', '5.0.7-6', '7.0.8-5', '7.0.12-7'];
 
-  const allowedVersions = ['6.0.5-4', '6.0.4-3', '5.0.7-6', '7.0.8-5', "7.0.12-7"]
-
-  delete engineData.status
-  engineData.spec.allowedVersions = allowedVersions
+  delete engineData.status;
+  engineData.spec.allowedVersions = allowedVersions;
 
   const updateResponse = await request.put(`/v1/namespaces/${testsNs}/database-engines/percona-server-mongodb-operator`, {
     data: engineData,
-  })
+  });
 
-  await checkError(updateResponse)
+  await checkError(updateResponse);
 
-  engineResponse = await request.get(`/v1/namespaces/${testsNs}/database-engines/percona-server-mongodb-operator`)
-  await checkError(engineResponse)
+  engineResponse = await request.get(`/v1/namespaces/${testsNs}/database-engines/percona-server-mongodb-operator`);
+  await checkError(engineResponse);
 
-  expect((await engineResponse.json()).spec.allowedVersions).toEqual(allowedVersions)
-})
+  expect((await engineResponse.json() as DatabaseEngineResponse).spec.allowedVersions).toEqual(allowedVersions);
+});
