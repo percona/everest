036eac9d3eb11d3da5b52893dceba38b875f4335
Mayank Shah <mayank.shah@percona.com>
2024-09-18 12:10:43 +0530

    Another refactor
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- api/database_cluster_backup.go
+++ api/database_cluster_backup.go
@@ -49,7 +49,7 @@ const (
 var everestAPIConstantBackoff = backoff.WithMaxRetries(backoff.NewConstantBackOff(time.Second), maxRetries)
 
 func (e *EverestServer) enforceDBBackupsRBAC(user string, bkp *everestv1alpha1.DatabaseClusterBackup) error {
-	if err := e.enforceOrErr(user, rbac.ResourceBackupStorages, rbac.ActionRead, rbac.RBACName(bkp.GetNamespace(), bkp.Spec.BackupStorageName)); err != nil {
+	if err := e.enforceOrErr(user, rbac.ResourceBackupStorages, rbac.ActionRead, rbac.ObjectName(bkp.GetNamespace(), bkp.Spec.BackupStorageName)); err != nil {
 		if errors.Is(err, errInsufficientPermissions) {
 			e.l.Error(errors.Join(err, errors.New("failed to check backup-storage permissions")))
 		}
--- api/validation.go
+++ api/validation.go
@@ -627,7 +627,7 @@ func (e *EverestServer) validateDatabaseClusterOnCreate(
 	// have permissions to take backups.
 	schedules := pointer.Get(pointer.Get(pointer.Get(databaseCluster.Spec).Backup).Schedules)
 	if len(schedules) > 0 {
-		if err := e.enforceOrErr(user, rbac.ResourceDatabaseClusterBackups, rbac.ActionCreate, rbac.RBACName(namespace, "")); err != nil {
+		if err := e.enforceOrErr(user, rbac.ResourceDatabaseClusterBackups, rbac.ActionCreate, rbac.ObjectName(namespace, "")); err != nil {
 			return err
 		}
 	}
@@ -1162,7 +1162,7 @@ func (e *EverestServer) validateBackupScheduledUpdate(
 	// If the schedules are updated, we need to check that the user has
 	// permission to create backups in this namespace.
 	if !isSchedulesEqual() {
-		if err := e.enforceOrErr(user, rbac.ResourceDatabaseClusterBackups, rbac.ActionCreate, rbac.RBACName(oldDB.GetNamespace(), "")); err != nil {
+		if err := e.enforceOrErr(user, rbac.ResourceDatabaseClusterBackups, rbac.ActionCreate, rbac.ObjectName(oldDB.GetNamespace(), "")); err != nil {
 			return err
 		}
 	}
