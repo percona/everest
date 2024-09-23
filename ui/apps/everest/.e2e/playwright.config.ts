094534f88d5719307d4c016b1f17a0d0c0fc513d
yurkovychv <vasyl.yurkovych@percona.com>
2024-09-23 14:01:38 +0300

    EVEREST-1471 initial tests for upgrade

--- ui/apps/everest/.e2e/playwright.config.ts
+++ ui/apps/everest/.e2e/playwright.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.EVEREST_URL || 'http://localhost:3000',
     extraHTTPHeaders: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
