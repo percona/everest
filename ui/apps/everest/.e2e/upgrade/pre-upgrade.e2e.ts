528e515cd846b249426b42d6413fa2b86ec4b7d5
yurkovychv <yurkovychv@users.noreply.github.com>
2024-09-23 07:52:06 +0000

    chore: lint/format

--- ui/apps/everest/.e2e/upgrade/pre-upgrade.e2e.ts
+++ ui/apps/everest/.e2e/upgrade/pre-upgrade.e2e.ts
@@ -1,38 +1,37 @@
-import { test } from "@playwright/test";
-import { createDbClusterFn } from "../utils/db-cluster";
-import {mongoDBCluster, postgresDBCluster, psDBCluster} from "./testData";
+import { test } from '@playwright/test';
+import { createDbClusterFn } from '../utils/db-cluster';
+import { mongoDBCluster, postgresDBCluster, psDBCluster } from './testData';
 
 test.describe.configure({ retries: 0 });
-test("Pre upgrade setup",{ tag: '@pre-upgrade' },
-    async ({ request}) => {
-    await createDbClusterFn(request, {
-        dbName: psDBCluster.name,
-        dbType: 'mysql',
-        numberOfNodes: psDBCluster.numberOfNodes,
-        cpu: psDBCluster.cpu,
-        disk: psDBCluster.disk,
-        memory: psDBCluster.memory,
-        externalAccess: psDBCluster.externalAccess,
-        sourceRanges: psDBCluster.sourceRanges,
-    });
+test('Pre upgrade setup', { tag: '@pre-upgrade' }, async ({ request }) => {
+  await createDbClusterFn(request, {
+    dbName: psDBCluster.name,
+    dbType: 'mysql',
+    numberOfNodes: psDBCluster.numberOfNodes,
+    cpu: psDBCluster.cpu,
+    disk: psDBCluster.disk,
+    memory: psDBCluster.memory,
+    externalAccess: psDBCluster.externalAccess,
+    sourceRanges: psDBCluster.sourceRanges,
+  });
 
-    await createDbClusterFn(request, {
-        dbName: mongoDBCluster.name,
-        dbType: 'mongodb',
-        numberOfNodes: mongoDBCluster.numberOfNodes,
-        cpu: mongoDBCluster.cpu,
-        disk: mongoDBCluster.disk,
-        memory: mongoDBCluster.disk,
-        externalAccess: mongoDBCluster.externalAccess,
-    });
+  await createDbClusterFn(request, {
+    dbName: mongoDBCluster.name,
+    dbType: 'mongodb',
+    numberOfNodes: mongoDBCluster.numberOfNodes,
+    cpu: mongoDBCluster.cpu,
+    disk: mongoDBCluster.disk,
+    memory: mongoDBCluster.disk,
+    externalAccess: mongoDBCluster.externalAccess,
+  });
 
-    await createDbClusterFn(request, {
-        dbName: postgresDBCluster.name,
-        dbType: 'postgresql',
-        numberOfNodes: postgresDBCluster.numberOfNodes,
-        cpu: postgresDBCluster.cpu,
-        disk: postgresDBCluster.disk,
-        memory: postgresDBCluster.disk,
-        externalAccess: postgresDBCluster.externalAccess,
-    });
+  await createDbClusterFn(request, {
+    dbName: postgresDBCluster.name,
+    dbType: 'postgresql',
+    numberOfNodes: postgresDBCluster.numberOfNodes,
+    cpu: postgresDBCluster.cpu,
+    disk: postgresDBCluster.disk,
+    memory: postgresDBCluster.disk,
+    externalAccess: postgresDBCluster.externalAccess,
+  });
 });
