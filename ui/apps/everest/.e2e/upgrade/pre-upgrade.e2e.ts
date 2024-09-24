ff5b036acc36dfaa739e938d7ac0bf06969a8160
yurkovychv <yurkovychv@users.noreply.github.com>
2024-09-24 09:34:00 +0000

    chore: lint/format

--- ui/apps/everest/.e2e/upgrade/pre-upgrade.e2e.ts
+++ ui/apps/everest/.e2e/upgrade/pre-upgrade.e2e.ts
@@ -5,50 +5,54 @@ import { getDBClustersList } from '../utils/db-clusters-list';
 import { TIMEOUT } from '../constants';
 
 test.describe.configure({ retries: 0 });
-test('Pre upgrade setup', { tag: '@pre-upgrade' }, async ({ page, request }) => {
-  await createDbClusterFn(request, {
-    dbName: psDBCluster.name,
-    dbType: 'mysql',
-    numberOfNodes: psDBCluster.numberOfNodes,
-    cpu: psDBCluster.cpu,
-    disk: psDBCluster.disk,
-    memory: psDBCluster.memory,
-    externalAccess: psDBCluster.externalAccess,
-    sourceRanges: psDBCluster.sourceRanges,
-  });
-
-  await createDbClusterFn(request, {
-    dbName: mongoDBCluster.name,
-    dbType: 'mongodb',
-    numberOfNodes: mongoDBCluster.numberOfNodes,
-    cpu: mongoDBCluster.cpu,
-    disk: mongoDBCluster.disk,
-    memory: mongoDBCluster.disk,
-    externalAccess: mongoDBCluster.externalAccess,
-  });
-
-  await createDbClusterFn(request, {
-    dbName: postgresDBCluster.name,
-    dbType: 'postgresql',
-    numberOfNodes: postgresDBCluster.numberOfNodes,
-    cpu: postgresDBCluster.cpu,
-    disk: postgresDBCluster.disk,
-    memory: postgresDBCluster.disk,
-    externalAccess: postgresDBCluster.externalAccess,
-  });
-
-  await page.waitForTimeout(TIMEOUT.TenSeconds)
-  const clusters = (await getDBClustersList(request)).items;
-  console.log(clusters);
-
-  await expect(async () => {
-    const clusters = (await getDBClustersList(request)).items;
-
-    clusters.map((c) => {
-      expect(c).toMatchObject({ status: { status: 'ready' } });
+test(
+  'Pre upgrade setup',
+  { tag: '@pre-upgrade' },
+  async ({ page, request }) => {
+    await createDbClusterFn(request, {
+      dbName: psDBCluster.name,
+      dbType: 'mysql',
+      numberOfNodes: psDBCluster.numberOfNodes,
+      cpu: psDBCluster.cpu,
+      disk: psDBCluster.disk,
+      memory: psDBCluster.memory,
+      externalAccess: psDBCluster.externalAccess,
+      sourceRanges: psDBCluster.sourceRanges,
     });
-  }).toPass({
-    timeout: TIMEOUT.TenMinutes,
-    intervals: [TIMEOUT.OneMinute],
-  });
-});
+
+    await createDbClusterFn(request, {
+      dbName: mongoDBCluster.name,
+      dbType: 'mongodb',
+      numberOfNodes: mongoDBCluster.numberOfNodes,
+      cpu: mongoDBCluster.cpu,
+      disk: mongoDBCluster.disk,
+      memory: mongoDBCluster.disk,
+      externalAccess: mongoDBCluster.externalAccess,
+    });
+
+    await createDbClusterFn(request, {
+      dbName: postgresDBCluster.name,
+      dbType: 'postgresql',
+      numberOfNodes: postgresDBCluster.numberOfNodes,
+      cpu: postgresDBCluster.cpu,
+      disk: postgresDBCluster.disk,
+      memory: postgresDBCluster.disk,
+      externalAccess: postgresDBCluster.externalAccess,
+    });
+
+    await page.waitForTimeout(TIMEOUT.TenSeconds);
+    const clusters = (await getDBClustersList(request)).items;
+    console.log(clusters);
+
+    await expect(async () => {
+      const clusters = (await getDBClustersList(request)).items;
+
+      clusters.map((c) => {
+        expect(c).toMatchObject({ status: { status: 'ready' } });
+      });
+    }).toPass({
+      timeout: TIMEOUT.TenMinutes,
+      intervals: [TIMEOUT.OneMinute],
+    });
+  }
+);
