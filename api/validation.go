44a35ece355cd9bbe370b20a359f2e395d5c9f4a
Oksana Grishchenko <oksana.grishchenko@percona.com>
2024-09-27 15:10:54 +0200

    test

--- api/validation.go
+++ api/validation.go
@@ -1390,7 +1390,7 @@ func validateMetadata(metadata *map[string]interface{}) error {
 	}
 	m := *metadata
 	if _, err := strconv.ParseUint(fmt.Sprint(m["resourceVersion"]), 10, 64); err != nil {
-		return errInvalidResourceVersion
+		return err
 	}
 	return nil
 }
