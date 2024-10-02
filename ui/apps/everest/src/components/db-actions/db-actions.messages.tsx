d65eafd8eeb5ee52fa6d9caaa9b94b5cb9563141
Diana Birsan <diana.birsan@3pillarglobal.com>
2024-10-02 18:12:04 +0300

    fix: remove unnecessary consts

--- ui/apps/everest/src/components/db-actions/db-actions.messages.tsx
+++ ui/apps/everest/src/components/db-actions/db-actions.messages.tsx
@@ -1,13 +1,4 @@
 export const Messages = {
-  onDemandBackupModal: {
-    subHead:
-      'Create an immediate backup of this database, to store a snapshot of its current data.',
-    backupName: 'Backup name',
-    storageLocation: 'Backup storage',
-    backupDetails: 'Backup details',
-    headerMessage: 'Create backup now',
-    submitMessage: 'Create',
-  },
   menuItems: {
     edit: 'Edit',
     delete: 'Delete',
@@ -17,10 +8,6 @@ export const Messages = {
     restoreFromBackup: 'Restore from a backup',
     createNewDbFromBackup: 'Create DB from a backup',
     dbStatusDetails: 'View DB status details',
-  },
-  schedulesBackupModal: {
-    subHead:
-      'Create a task that regularly backs up this database according to your specified schedule.',
   },
   deleteModal: {
     header: 'Delete database',
@@ -38,18 +25,4 @@ export const Messages = {
       'Backups storage data is kept for PostgreSQL databases.',
     confirmButtom: 'Delete',
   },
-  noStorages: {
-    alert:
-      'You don’t have any backup storage set yet. Please set one before you proceed.',
-    submitButton: 'Go to settings',
-    cancel: 'Cancel',
-  },
-
-  backups: 'Backups',
-  restores: 'Restores',
-  overview: 'Overview',
-  components: 'Components',
-  dbActions: 'Actions',
-  restoringDb:
-    'We are recovering your database. Do not perform any actions on the database until recovery is complete.',
 };
