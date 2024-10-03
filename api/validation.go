2c014dc53d7085e35dbf7f645cab6faacf7686c2
Diogo Recharte <diogo.recharte@percona.com>
2024-10-03 16:25:35 +0000

    EVEREST-1544 fix minimum proxy replicas for PXC (#735)

--- api/validation.go
+++ api/validation.go
@@ -66,6 +66,7 @@ const (
 	minConfigServersNumNNodeReplset = 3
 	minConfigServersNum1NodeReplset = 1
 	maxPXCEngineReplicas            = 5
+	minPXCProxyReplicas             = 2
 )
 
 var (
@@ -126,6 +127,7 @@ var (
 	errShardingVersion               = errors.New("sharding is available starting PSMDB 1.17.0")
 	errEvenEngineReplicas            = errors.New("engine replicas number should be odd")
 	errMaxPXCEngineReplicas          = errors.New("max replicas number for MySQL is 5")
+	errMinPXCProxyReplicas           = errors.New("min replicas number for Proxy is 2")
 
 	//nolint:gochecknoglobals
 	operatorEngine = map[everestv1alpha1.EngineType]string{
@@ -702,7 +704,7 @@ func (e *EverestServer) validateDatabaseClusterCR(
 		return err
 	}
 	if databaseCluster.Spec.Proxy != nil && databaseCluster.Spec.Proxy.Type != nil {
-		if err := validateProxy(databaseCluster.Spec.Engine.Type, string(*databaseCluster.Spec.Proxy.Type)); err != nil {
+		if err := validateProxy(databaseCluster); err != nil {
 			return err
 		}
 	}
@@ -918,7 +920,22 @@ func containsVersion(version string, versions []string) bool {
 	return false
 }
 
-func validateProxy(engineType DatabaseClusterSpecEngineType, proxyType string) error {
+func validateProxy(databaseCluster *DatabaseCluster) error {
+	if err := validateProxyType(databaseCluster.Spec.Engine.Type, string(*databaseCluster.Spec.Proxy.Type)); err != nil {
+		return err
+	}
+
+	if databaseCluster.Spec.Engine.Type == DatabaseClusterSpecEngineType(everestv1alpha1.DatabaseEnginePXC) {
+		if databaseCluster.Spec.Engine.Replicas != nil && *databaseCluster.Spec.Engine.Replicas > 1 &&
+			databaseCluster.Spec.Proxy.Replicas != nil && *databaseCluster.Spec.Proxy.Replicas < minPXCProxyReplicas {
+			return errMinPXCProxyReplicas
+		}
+	}
+
+	return nil
+}
+
+func validateProxyType(engineType DatabaseClusterSpecEngineType, proxyType string) error {
 	if engineType == DatabaseClusterSpecEngineType(everestv1alpha1.DatabaseEnginePXC) {
 		if proxyType != string(everestv1alpha1.ProxyTypeProxySQL) && proxyType != string(everestv1alpha1.ProxyTypeHAProxy) {
 			return errUnsupportedPXCProxy
