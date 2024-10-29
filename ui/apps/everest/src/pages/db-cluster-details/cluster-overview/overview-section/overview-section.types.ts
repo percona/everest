da9b03b96852cb72573dc942683be46e4ba3bf40
Diana Birsan <diana.birsan@3pillarglobal.com>
2024-10-29 12:53:44 +0200

    feat: add backups to overview card

--- ui/apps/everest/src/pages/db-cluster-details/cluster-overview/overview-section/overview-section.types.ts
+++ ui/apps/everest/src/pages/db-cluster-details/cluster-overview/overview-section/overview-section.types.ts
@@ -1,8 +1,9 @@
 import { LoadableChildrenProps } from '@percona/ui-lib';
 import { ButtonProps } from '@mui/material';
+import { ReactNode } from 'react';
 
 export type OverviewSectionProps = {
-  title: string;
+  title: string | ReactNode;
   dataTestId?: string;
   actionButtonProps?: ButtonProps;
   editable?: boolean;
