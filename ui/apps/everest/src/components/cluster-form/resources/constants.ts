48290cac9e162a209157b70ed41ae3e35f1dc2c3
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2024-10-30 12:10:53 +0400

    EVEREST-1597: fix of sharding validation in overview edit

--- ui/apps/everest/src/components/cluster-form/resources/constants.ts
+++ ui/apps/everest/src/components/cluster-form/resources/constants.ts
@@ -184,6 +184,8 @@ const numberOfResourcesValidator = (
 export const resourcesFormSchema = (passthrough?: boolean) => {
   const objectShape = {
     [DbWizardFormFields.shardNr]: z.string().optional(),
+    // solves the validation problem in a separate form
+    [DbWizardFormFields.sharding]: z.boolean().optional(),
     [DbWizardFormFields.shardConfigServers]: z.string().optional(),
     [DbWizardFormFields.cpu]: resourceToNumber(0.6),
     [DbWizardFormFields.memory]: resourceToNumber(0.512),
