faacc93bed812a1cf20b6fce2d10d3e9299a2099
Mayank Shah <mayank.shah@percona.com>
2024-11-13 16:51:02 +0530

    fix cli test
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- cli-tests/tests/flow/pg-operator.spec.ts
+++ cli-tests/tests/flow/pg-operator.spec.ts
@@ -95,7 +95,7 @@ test.describe('Everest CLI install', async () => {
       await out.assertSuccess();
       await out.outErrContainsNormalizedMany([
         'percona-xtradb-cluster-operator operator has been installed',
-        'everest-operator operator has been installed',
+        'Deployment \'everest-operator\' in namespace \'everest-system\' is ready',
       ]);
       await out.outNotContains([
         'Connected Kubernetes cluster to Everest',
