2336afe8f9cbe880db60826cdc207464ebc5b234
Mayank Shah <mayank.shah@percona.com>
2024-12-03 15:24:16 +0530

    EVEREST-1673 | New CLI command for managing DB namespaces (#869)
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

---
+++ pkg/cli/helm/flags.go
@@ -0,0 +1,12 @@
+package helm
+
+const (
+	// FlagChartDir is a CLI flag for specifying the chart directory.
+	FlagChartDir = "chart-dir"
+	// FlagRepository is a CLI flag for specifying the Helm repository.
+	FlagRepository = "repository"
+	// FlagHelmSet is a CLI flag for setting Helm values.
+	FlagHelmSet = "helm-set"
+	// FlagHelmValues is a CLI flag for specifying Helm values files.
+	FlagHelmValues = "helm-values"
+)
