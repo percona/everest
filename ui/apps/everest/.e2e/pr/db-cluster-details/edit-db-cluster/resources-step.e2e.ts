e4e18619ec1ccc0a4a749bd7719660b39cd42377
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2024-12-04 18:50:19 +0400

    EVEREST-1065-remove-edit-button: fix e2e tests conflicts

--- ui/apps/everest/.e2e/pr/db-cluster-details/edit-db-cluster/resources-step.e2e.ts
+++ ui/apps/everest/.e2e/pr/db-cluster-details/edit-db-cluster/resources-step.e2e.ts
@@ -25,6 +25,7 @@ import {
   submitWizard,
 } from '@e2e/utils/db-wizard';
 import { waitForDelete, waitForStatus } from '@e2e/utils/table';
+import { selectDbEngine } from '@e2e/pr/db-cluster/db-wizard/db-wizard-utils';
 
 let token: string;
 
@@ -39,200 +40,192 @@ const openResourcesModal = async (page: Page) => {
   { db: 'psmdb', size: 1 },
   { db: 'pxc', size: 1 },
   // { db: 'postgresql', size: 1 },
-].forEach(({ db, size }) => {
-  test.describe(`Overview page: ${db} resources editing`, () => {
-    test.describe.configure({ timeout: 900000 });
+].forEach(
+  ({ db, size }: { db: 'pxc' | 'psmdb' | 'postgresql'; size: number }) => {
+    test.describe(`Overview page: ${db} resources editing`, () => {
+      test.describe.configure({ timeout: 900000 });
 
-    const clusterName = `${db}-${size}-resources-edit`;
+      const clusterName = `${db}-${size}-resources-edit`;
 
-    let storageClasses = [];
+      let storageClasses = [];
 
-    test.beforeAll(async ({ request }) => {
-      token = await getTokenFromLocalStorage();
+      test.beforeAll(async ({ request }) => {
+        token = await getTokenFromLocalStorage();
 
-      const { storageClassNames = [] } = await getClusterDetailedInfo(
-        token,
-        request
-      );
-      storageClasses = storageClassNames;
-    });
-
-    test(`Creation and waiting of ready ${size} node ${db} ${db == 'psmdb' ? 'with sharding' : ''} for resources edit tests`, async ({
-      page,
-    }) => {
-      expect(storageClasses.length).toBeGreaterThan(0);
-      await page.goto('/databases/new');
-
-      await test.step('Populate basic info', async () => {
-        await page.getByTestId('toggle-button-group-input-db-type').waitFor();
-        await populateBasicInformation(
-          page,
-          db,
-          storageClasses[0],
-          clusterName
+        const { storageClassNames = [] } = await getClusterDetailedInfo(
+          token,
+          request
         );
-
-        // sharding enabling
-        if (db == 'psmdb') {
-          await page.getByTestId('switch-input-sharding').click();
-        }
-
-        // go to resources page
-        await moveForward(page);
+        storageClasses = storageClassNames;
       });
 
-      await test.step('Populate resources', async () => {
-        await page
-          .getByRole('button')
-          .getByText(size + ' node')
-          .click();
-        await expect(page.getByText('Nº nodes: ' + size)).toBeVisible();
+      test(`Creation and waiting of ready ${size} node ${db} ${db == 'psmdb' ? 'with sharding' : ''} for resources edit tests`, async ({
+        page,
+      }) => {
+        expect(storageClasses.length).toBeGreaterThan(0);
+        await page.goto('/databases');
+        await selectDbEngine(page, db);
+
+        await test.step('Populate basic info', async () => {
+          await page.getByTestId('text-input-db-name').fill(clusterName);
+          // sharding enabling
+          if (db == 'psmdb') {
+            await page.getByTestId('switch-input-sharding').click();
+          }
+
+          // go to resources page
+          await moveForward(page);
+        });
+
+        await test.step('Populate resources', async () => {
+          await page
+            .getByRole('button')
+            .getByText(size + ' node')
+            .click();
+          await expect(page.getByText('Nº nodes: ' + size)).toBeVisible();
+        });
+
+        await test.step('Move forward form with default values', async () => {
+          //go to backups page
+          await moveForward(page);
+          //go to advanced configuration
+          await moveForward(page);
+          //go to monitoring
+          await moveForward(page);
+        });
+
+        await test.step('Submit form', async () => {
+          await submitWizard(page);
+          await expect(
+            page.getByText('Awesome! Your database is being created!')
+          ).toBeVisible();
+        });
       });
 
-      await test.step('Move forward form with default values', async () => {
-        //go to backups page
-        await moveForward(page);
-        //go to advanced configuration
-        await moveForward(page);
-        //go to monitoring
-        await moveForward(page);
+      test(`Show the correct default values during editing of ${db}`, async ({
+        page,
+      }) => {
+        await page.goto('/databases');
+
+        await findDbAndClickRow(page, clusterName);
+        await test.step('Open edit resource modal', async () => {
+          openResourcesModal(page);
+        });
+
+        await test.step('Check default values', async () => {
+          //TODO improve setting during creation the CPU, memory, etc.
+          //TODO customize sharding number, number of config servers
+          await expect(
+            page.getByTestId(`toggle-button-nodes-${size}`)
+          ).toHaveAttribute('aria-pressed', 'true');
+          await expect(
+            page.getByTestId('node-resources-toggle-button-small')
+          ).toHaveAttribute('aria-pressed', 'true');
+
+          await page.getByTestId('proxies-accordion').click();
+          if (db != 'psmdb') {
+            await expect(
+              page.getByTestId(`toggle-button-proxies-${size}`)
+            ).toHaveAttribute('aria-pressed', 'true');
+          } else {
+            // sharding
+            await expect(
+              page.getByTestId(`toggle-button-routers-${size}`)
+            ).toHaveAttribute('aria-pressed', 'true');
+            expect(
+              await page.getByTestId('text-input-shard-nr').inputValue()
+            ).toBe('2');
+            await expect(
+              page.getByTestId('shard-config-servers-1')
+            ).toHaveAttribute('aria-pressed', 'true');
+          }
+        });
       });
 
-      await test.step('Submit form', async () => {
-        await submitWizard(page);
+      test(`Disk resize during edition for ${db} should be disabled`, async ({
+        page,
+      }) => {
+        await page.goto('/databases');
+        await findDbAndClickRow(page, clusterName);
+
+        await test.step('Open edit resource modal', async () => {
+          openResourcesModal(page);
+        });
+        await expect(page.getByTestId('text-input-disk')).toBeDisabled();
+      });
+
+      test('Set custom resources to nodes and proxies during editing', async ({
+        page,
+      }) => {
+        await page.goto('/databases');
+        await findDbAndClickRow(page, clusterName);
+        await test.step('Open edit resource modal', async () => {
+          openResourcesModal(page);
+        });
+
+        await test.step('Set custom resources size per node', async () => {
+          await expect(
+            page.getByTestId('node-resources-toggle-button-custom')
+          ).toHaveAttribute('aria-pressed', 'false');
+          page.getByTestId('text-input-cpu').fill('2');
+          await expect(
+            page.getByTestId('node-resources-toggle-button-custom')
+          ).toHaveAttribute('aria-pressed', 'true');
+        });
+
+        //TODO can be better customizable between different dbTypes
+        await page.getByTestId('proxies-accordion').click();
+        await test.step(`Set custom number of ${db != 'psmdb' ? 'proxies' : 'routers'}`, async () => {
+          await page
+            .getByTestId(
+              `toggle-button-${db != 'psmdb' ? 'proxies' : 'routers'}-custom`
+            )
+            .click();
+          await page.getByTestId('text-input-custom-nr-of-proxies').fill('2');
+        });
+
+        await test.step(`Set custom resources size per ${db != 'psmdb' ? 'proxies' : 'routers'}`, async () => {
+          await expect(
+            page.getByTestId(
+              `${db != 'psmdb' ? 'proxy' : 'router'}-resources-toggle-button-custom`
+            )
+          ).toHaveAttribute('aria-pressed', 'false');
+          page
+            .getByTestId('text-input-proxy-cpu')
+            .fill(`${db != 'psmdb' ? '0.4' : '2'}`);
+          await expect(
+            page.getByTestId(
+              `${db != 'psmdb' ? 'proxy' : 'router'}-resources-toggle-button-custom`
+            )
+          ).toHaveAttribute('aria-pressed', 'true');
+        });
+
+        expect(page.getByTestId('form-dialog-save')).not.toBeDisabled();
+        await page.getByTestId('form-dialog-save').click();
+
+        //check result
         await expect(
-          page.getByText('Awesome! Your database is being created!')
+          page
+            .getByTestId('node-cpu-overview-section-row')
+            .filter({ hasText: '2 CPU' })
+        ).toBeVisible();
+
+        await expect(
+          page
+            .getByTestId(
+              `${db != 'psmdb' ? 'proxies' : 'routers'}-cpu-overview-section-row`
+            )
+            .filter({
+              hasText: `${db != 'psmdb' ? '2 x 0.4 CPU = 0.80' : '2 x 2 CPU = 4.00'} CPU`,
+            })
         ).toBeVisible();
       });
-    });
 
-    test(`Show the correct default values during editing of ${db}`, async ({
-      page,
-    }) => {
-      await page.goto('/databases');
-
-      await findDbAndClickRow(page, clusterName);
-      await test.step('Open edit resource modal', async () => {
-        openResourcesModal(page);
-      });
-
-      await test.step('Check default values', async () => {
-        //TODO improve setting during creation the CPU, memory, etc.
-        //TODO customize sharding number, number of config servers
-        await expect(
-          page.getByTestId(`toggle-button-nodes-${size}`)
-        ).toHaveAttribute('aria-pressed', 'true');
-        await expect(
-          page.getByTestId('node-resources-toggle-button-small')
-        ).toHaveAttribute('aria-pressed', 'true');
-
-        await page.getByTestId('proxies-accordion').click();
-        if (db != 'psmdb') {
-          await expect(
-            page.getByTestId(`toggle-button-proxies-${size}`)
-          ).toHaveAttribute('aria-pressed', 'true');
-        } else {
-          // sharding
-          await expect(
-            page.getByTestId(`toggle-button-routers-${size}`)
-          ).toHaveAttribute('aria-pressed', 'true');
-          expect(
-            await page.getByTestId('text-input-shard-nr').inputValue()
-          ).toBe('2');
-          await expect(
-            page.getByTestId('shard-config-servers-1')
-          ).toHaveAttribute('aria-pressed', 'true');
-        }
+      test(`Delete cluster [${db} size ${size}]`, async ({ page }) => {
+        await deleteDbCluster(page, clusterName);
+        await waitForStatus(page, clusterName, 'Deleting', 15000);
+        await waitForDelete(page, clusterName, 160000);
       });
     });
-
-    test(`Disk resize during edition for ${db} should be disabled`, async ({
-      page,
-    }) => {
-      await page.goto('/databases');
-      await findDbAndClickRow(page, clusterName);
-
-      await test.step('Open edit resource modal', async () => {
-        openResourcesModal(page);
-      });
-      await expect(page.getByTestId('text-input-disk')).toBeDisabled();
-    });
-
-    test('Set custom resources to nodes and proxies during editing', async ({
-      page,
-    }) => {
-      await page.goto('/databases');
-      await findDbAndClickRow(page, clusterName);
-      await test.step('Open edit resource modal', async () => {
-        openResourcesModal(page);
-      });
-
-      await test.step('Set custom resources size per node', async () => {
-        await expect(
-          page.getByTestId('node-resources-toggle-button-custom')
-        ).toHaveAttribute('aria-pressed', 'false');
-        page.getByTestId('text-input-cpu').fill('2');
-        await expect(
-          page.getByTestId('node-resources-toggle-button-custom')
-        ).toHaveAttribute('aria-pressed', 'true');
-      });
-
-      //TODO can be better customizable between different dbTypes
-      await page.getByTestId('proxies-accordion').click();
-      await test.step(`Set custom number of ${db != 'psmdb' ? 'proxies' : 'routers'}`, async () => {
-        await page
-          .getByTestId(
-            `toggle-button-${db != 'psmdb' ? 'proxies' : 'routers'}-custom`
-          )
-          .click();
-        await page.getByTestId('text-input-custom-nr-of-proxies').fill('2');
-      });
-
-      await test.step(`Set custom resources size per ${db != 'psmdb' ? 'proxies' : 'routers'}`, async () => {
-        await expect(
-          page.getByTestId(
-            `${db != 'psmdb' ? 'proxy' : 'router'}-resources-toggle-button-custom`
-          )
-        ).toHaveAttribute('aria-pressed', 'false');
-        page
-          .getByTestId('text-input-proxy-cpu')
-          .fill(`${db != 'psmdb' ? '0.4' : '2'}`);
-        await expect(
-          page.getByTestId(
-            `${db != 'psmdb' ? 'proxy' : 'router'}-resources-toggle-button-custom`
-          )
-        ).toHaveAttribute('aria-pressed', 'true');
-      });
-
-      expect(page.getByTestId('form-dialog-save')).not.toBeDisabled();
-      await page.getByTestId('form-dialog-save').click();
-
-      //check result
-
-      await page.pause();
-      await expect(
-        page
-          .getByTestId('node-cpu-overview-section-row')
-          .filter({ hasText: '2 CPU' })
-      ).toBeVisible();
-
-      await expect(
-        page
-          .getByTestId(
-            `${db != 'psmdb' ? 'proxies' : 'routers'}-cpu-overview-section-row`
-          )
-          .filter({
-            hasText: `${db != 'psmdb' ? '2 x 0.4 CPU = 0.80' : '2 x 2 CPU = 4.00'} CPU`,
-          })
-      ).toBeVisible();
-
-      await page.pause();
-    });
-
-    test(`Delete cluster [${db} size ${size}]`, async ({ page }) => {
-      await deleteDbCluster(page, clusterName);
-      await waitForStatus(page, clusterName, 'Deleting', 15000);
-      await waitForDelete(page, clusterName, 160000);
-    });
-  });
-});
+  }
+);
