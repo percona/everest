8ba4551100ab13ea00c3a82c44babef6c1cb45ea
Diogo Recharte <diogo.recharte@percona.com>
2024-12-06 12:28:15 +0000

    EVEREST-1713 fix support link

--- ui/apps/everest/src/pages/common/empty-state/ContactSupportLink.tsx
+++ ui/apps/everest/src/pages/common/empty-state/ContactSupportLink.tsx
@@ -6,7 +6,7 @@ import { helpIconStyle } from './utils';
 export const ContactSupportLink = ({ msg }: { msg: string }) => {
   const theme = useTheme();
   return (
-    <Link target="_blank" rel="noopener" href="https://hubs.ly/Q02Rt6pG0">
+    <Link target="_blank" rel="noopener" href="https://hubs.ly/Q02YRLsL0">
       <Button startIcon={<HelpIcon sx={helpIconStyle(theme)} />}>{msg}</Button>
     </Link>
   );
