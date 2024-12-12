7ea90898e7bda9bd7b3c42f0a933e41210c85968
Fábio Silva <ffjs1993@gmail.com>
2024-12-12 11:09:37 +0000

    EVEREST-1737: add code commands to add namespaces (#923)
    * feat: add code commands to add namespaces
    
    * chore: add Roboto Mono

--- ui/apps/everest/src/pages/common/empty-state/namespaces/messages.ts
+++ ui/apps/everest/src/pages/common/empty-state/namespaces/messages.ts
@@ -1,7 +1,7 @@
 export const Messages = {
-  noNamespaces: 'Uh-oh! Looks like no namespaces have been created',
-  createToStart: 'Create one to get started.',
-  command: 'Run `everestctl namespaces add [Namespace]` command.',
+  noNamespaces:
+    'Uh-oh! Looks like no namespaces have been created or no database engines are available.',
+  createToStart: 'To create a namespace, follow the commands:',
   learnMore: 'Learn more',
   contactSupport: 'Contact Percona Support',
 };
