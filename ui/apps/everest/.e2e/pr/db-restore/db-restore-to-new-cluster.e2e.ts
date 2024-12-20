ac1df470ecdc3aa64cc6783010e40c8c95345d16
Fabio Silva <ffjs1993@gmail.com>
2024-12-20 12:22:32 +0000

    fix: restore version not editable

--- ui/apps/everest/.e2e/pr/db-restore/db-restore-to-new-cluster.e2e.ts
+++ ui/apps/everest/.e2e/pr/db-restore/db-restore-to-new-cluster.e2e.ts
@@ -29,6 +29,7 @@ test.describe('DB Cluster Restore to the new cluster', () => {
     await createDbClusterFn(request, {
       dbName: dbClusterName,
       dbType: 'mysql',
+      dbVersion: '8.0.36-28.1',
       numberOfNodes: '1',
       backup: {
         enabled: true,
@@ -61,7 +62,32 @@ test.describe('DB Cluster Restore to the new cluster', () => {
     ).toBeVisible();
   });
 
-  test('DB cluster detail restore action', async ({ page }) => {
+  test.only('DB cluster detail restore action', async ({ page }) => {
+    await page.route(
+      '/v1/namespaces/**/database-clusters/**/backups',
+      async (route) => {
+        await route.fulfill({
+          json: {
+            items: [
+              {
+                metadata: {
+                  name: 'backup-1',
+                },
+                spec: {
+                  dbClusterName,
+                  backupStorageName: getBucketNamespacesMap()[0][0],
+                },
+                status: {
+                  state: 'Succeeded',
+                  created: '2024-12-20T11:57:41Z',
+                  completed: '2024-12-20T11:58:07Z',
+                },
+              },
+            ],
+          },
+        });
+      }
+    );
     await findDbAndClickRow(page, dbClusterName);
     const actionButton = page.getByTestId('actions-button');
     await actionButton.click();
@@ -76,5 +102,15 @@ test.describe('DB Cluster Restore to the new cluster', () => {
         .getByTestId('select-backup-name-button')
         .getByText(Messages.selectBackup)
     ).toBeVisible();
+    await page.getByTestId('select-backup-name-button').click();
+    await page.getByText('backup-1').click();
+    await page.getByText('Create', { exact: true }).click();
+    await expect(
+      page.getByText('Basic information', { exact: true })
+    ).toBeVisible();
+    await expect(page.getByTestId('select-input-db-version')).toBeDisabled();
+    await expect(page.getByTestId('select-input-db-version')).toHaveValue(
+      '8.0.36-28.1'
+    );
   });
 });
