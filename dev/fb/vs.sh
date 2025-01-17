0e1c8d5c3bc47c897c853da3981fa324e1d01038
Diogo Recharte <diogo.recharte@percona.com>
2025-01-17 21:44:49 +0000

    EVEREST-107 Simplify steps to run a FB
    By embedding all the artifacts in a single directory we can remove the
    complexity of setting the env vars.

--- dev/fb/vs.sh
+++ dev/fb/vs.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ -z "$VS_IMAGE" ]]; then
-    echo "Error: VS_IMAGE environment variable is empty." >&2
-    exit 1
-fi
-
-curl https://raw.githubusercontent.com/Percona-Lab/percona-version-service/main/deploy.yaml  > vs_deploy.yaml
-
-# use FB VS image in the VS configuration
-if [[ "$(uname)" == "Darwin" ]]; then
-  sed -i '' "s/perconalab\/version-service:.*/perconalab\/version-service:$VS_IMAGE/g" vs_deploy.yaml
-else
-  sed -i "s/perconalab\/version-service:.*/perconalab\/version-service:$VS_IMAGE/g" vs_deploy.yaml
-fi
-
-
 # deploy VS
 kubectl apply -f vs_deploy.yaml > /dev/null
 
