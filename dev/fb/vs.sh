0905b189e2c0c83d398e2509f18e082388b54e36
Diogo Recharte <diogo.recharte@percona.com>
2025-01-20 16:51:05 +0000

    EVEREST-107 increase FB VS ready timeout to 60s
    Sometimes the default 30s isn't enought and we are seeing
    error: readyReplicas is not found

--- dev/fb/vs.sh
+++ dev/fb/vs.sh
@@ -4,7 +4,7 @@
 kubectl apply -f vs_deploy.yaml > /dev/null
 
 # wait until the VS is ready
-kubectl wait --for=jsonpath='{.status.readyReplicas}'=3 deployment/percona-version-service > /dev/null
+kubectl wait --for=jsonpath='{.status.readyReplicas}'=3 deployment/percona-version-service --timeout 60s > /dev/null
 
 # get the internal IP of the VS
 echo "$(kubectl get service percona-version-service -o jsonpath='{.spec.clusterIP}')"
