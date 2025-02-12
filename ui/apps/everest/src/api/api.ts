fa0cb781ea8fe45a8ee799d73316b20e24756868
Fabio Silva <ffjs1993@gmail.com>
2025-02-12 12:46:22 +0000

    feat: logout on HTTP 400 JWT error

--- ui/apps/everest/src/api/api.ts
+++ ui/apps/everest/src/api/api.ts
@@ -12,11 +12,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { enqueueSnackbar } from 'notistack';
 
 const BASE_URL = '/v1/';
 const DEFAULT_ERROR_MESSAGE = 'Something went wrong';
+const MISSING_MALFORMED_JWT_MESSAGE = 'missing or malformed jwt';
 const MAX_ERROR_MESSAGE_LENGTH = 120;
 let errorInterceptor: number | null = null;
 let authInterceptor: number | null = null;
@@ -29,31 +30,31 @@ export const addApiErrorInterceptor = () => {
   if (errorInterceptor === null) {
     errorInterceptor = api.interceptors.response.use(
       (response) => response,
-      (error) => {
+      (error: AxiosError<{ message?: string }>) => {
         if (
           error.response &&
           error.response.status >= 400 &&
           error.response.status <= 500
         ) {
-          let message = DEFAULT_ERROR_MESSAGE;
-
+          let message = error.response.data.message ?? DEFAULT_ERROR_MESSAGE;
           if (error.response.status === 401) {
             localStorage.removeItem('everestToken');
             sessionStorage.clear();
             location.replace('/login');
           }
 
+          if (
+            error.response.status === 400 &&
+            message.includes(MISSING_MALFORMED_JWT_MESSAGE)
+          ) {
+            location.href = '/logout';
+            return;
+          }
+
           if (error.config?.disableNotifications !== true) {
-            if (error.response.data && error.response.data.message) {
-              if (
-                error.response.data.message.length > MAX_ERROR_MESSAGE_LENGTH
-              ) {
-                message = `${error.response.data.message
-                  .trim()
-                  .substring(0, MAX_ERROR_MESSAGE_LENGTH)}...`;
-              } else {
-                message = error.response.data.message.trim();
-              }
+            message = message.trim();
+            if (message.length > MAX_ERROR_MESSAGE_LENGTH) {
+              message = `${message.substring(0, MAX_ERROR_MESSAGE_LENGTH)}...`;
             }
 
             enqueueSnackbar(message, {
