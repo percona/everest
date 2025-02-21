8907bcd7a31ddcffabed1b2eaddad901b3c4936a
Diogo Recharte <diogo.recharte@percona.com>
2025-02-21 13:17:02 +0000

    EVEREST-107 linting

--- api-tests/eslint.config.mjs
+++ api-tests/eslint.config.mjs
@@ -6,13 +6,13 @@ import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+const __filename = fileURLToPath(import.meta.url),
+    __dirname = path.dirname(__filename),
+    compat = new FlatCompat({
+        baseDirectory: __dirname,
+        recommendedConfig: js.configs.recommended,
+        allConfig: js.configs.all
+    });
 
 export default [{
     ignores: ["node_modules", "playwright.config.ts"],
