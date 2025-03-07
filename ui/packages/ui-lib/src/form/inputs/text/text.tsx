48e57f7d2a8e9365c437e07b5170b7b69cd14844
Tikdev00 <tigran.papikyan@simbirsoft.com>
2025-03-07 18:39:20 +0400

    EVEREST-1901 | removing unnecessary return statement

--- ui/packages/ui-lib/src/form/inputs/text/text.tsx
+++ ui/packages/ui-lib/src/form/inputs/text/text.tsx
@@ -16,44 +16,42 @@ const TextInput = ({
     <Controller
       name={name}
       control={control ?? contextControl}
-      render={({ field, fieldState: { error } }) => {
-        return (
-          <TextField
-            label={label}
-            {...field}
-            size={textFieldProps?.size || 'small'}
-            sx={{ mt: 3 }}
-            {...textFieldProps}
-            variant="outlined"
-            required={isRequired}
-            error={!!error}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            inputProps={{
-              'data-testid': `text-input-${kebabize(name)}`,
-              onWheel: (e) => {
-                (e.target as HTMLElement).blur();
-              },
-              onChange: (event: React.ChangeEvent<HTMLInputElement>) => {
-                if (textFieldProps?.onChange) {
-                  const modifiedEvent = {
-                    ...event,
-                    target: {
-                      ...event.target,
-                      value: textFieldProps.onChange(event),
-                    },
-                  };
-                  field.onChange(modifiedEvent);
-                } else {
-                  field.onChange(event);
-                }
-              },
-            }}
-            helperText={error ? error.message : textFieldProps?.helperText}
-          />
-        );
-      }}
+      render={({ field, fieldState: { error } }) => (
+       <TextField
+          label={label}
+          {...field}
+          size={textFieldProps?.size || 'small'}
+          sx={{ mt: 3 }}
+          {...textFieldProps}
+          variant="outlined"
+          required={isRequired}
+          error={!!error}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          inputProps={{
+            'data-testid': `text-input-${kebabize(name)}`,
+            onWheel: (e) => {
+              (e.target as HTMLElement).blur();
+            },
+            onChange: (event: React.ChangeEvent<HTMLInputElement>) => {
+              if (textFieldProps?.onChange) {
+                const modifiedEvent = {
+                  ...event,
+                  target: {
+                    ...event.target,
+                    value: textFieldProps.onChange(event),
+                  },
+                };
+                field.onChange(modifiedEvent);
+              } else {
+                field.onChange(event);
+              }
+            },
+          }}
+          helperText={error ? error.message : textFieldProps?.helperText}
+        />
+  )}
       {...controllerProps}
     />
   );
