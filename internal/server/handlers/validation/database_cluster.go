25565973d1cb72d94d4294537398991e599798ed
Mayank Shah <mayank.shah@percona.com>
2025-03-19 15:35:50 +0530

    allow postgresql storage scaling
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- internal/server/handlers/validation/database_cluster.go
+++ internal/server/handlers/validation/database_cluster.go
@@ -540,7 +540,7 @@ func (h *validateHandler) validateDatabaseClusterOnUpdate(
 	}
 
 	// Do not allow updating storage size.
-	if dbc.Spec.Engine.Type != everestv1alpha1.DatabaseEnginePXC &&
+	if dbc.Spec.Engine.Type != everestv1alpha1.DatabaseEnginePostgresql &&
 		dbc.Spec.Engine.Storage.Size.Cmp(oldDB.Spec.Engine.Storage.Size) != 0 {
 		return errCannotChangeStorageSize
 	}
