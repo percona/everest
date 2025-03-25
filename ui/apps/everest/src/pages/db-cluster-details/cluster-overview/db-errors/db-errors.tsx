cb14ff4418d11111bde5368ccdad77c929729237
Diana Birsan <diana.birsan@3pillarglobal.com>
2025-03-25 21:11:03 +0200

    UI: Limit disk size values to only integer values (#1256)
    * fix: limit decimal values for disk size
    
    * feat: add alert for error and modal to see error message, enable disk size downscale if VolumeResizeFailed condition is present
    
    * fix: add missing prop to test
    
    * fix: missing prop on query update
    
    ---------
    
    Co-authored-by: Fabio Silva <ffjs1993@gmail.com>

---
+++ ui/apps/everest/src/pages/db-cluster-details/cluster-overview/db-errors/db-errors.tsx
@@ -0,0 +1,49 @@
+import { Alert, Box, Button, Typography } from '@mui/material';
+import { StatusCondition } from 'shared-types/dbCluster.types';
+import { humanizeDbError } from './constants';
+import { useState } from 'react';
+import { DbErrorDialog } from './db-error-dialog';
+import { Messages } from './messages';
+
+export const DbErrors = ({ conditions }: { conditions: StatusCondition[] }) => {
+  const [openDialog, setOpenDialog] = useState(false);
+
+  return (
+    <>
+      {conditions.map((condition) => {
+        return (
+          <Box>
+            <Alert
+              severity="error"
+              sx={{
+                width: '100%',
+                height: 'auto',
+                marginTop: 1,
+                marginBottom: 1,
+                alignItems: 'center',
+              }}
+              action={
+                <Button
+                  color="error"
+                  size="small"
+                  onClick={() => setOpenDialog(true)}
+                >
+                  {Messages.errorDetails}
+                </Button>
+              }
+            >
+              <Typography>{humanizeDbError(condition.type)}</Typography>
+            </Alert>
+
+            <DbErrorDialog
+              title={Messages.errorDetails}
+              content={condition.message}
+              open={openDialog}
+              onClose={() => setOpenDialog(false)}
+            />
+          </Box>
+        );
+      })}
+    </>
+  );
+};
