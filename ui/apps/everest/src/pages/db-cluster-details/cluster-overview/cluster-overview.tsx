b94efa8d824c8f7ad6c9ddb752cbe42c659495ae
Fabio Silva <ffjs1993@gmail.com>
2025-03-25 11:48:54 +0000

    feat: disable actions while upgrading or rescaling

--- ui/apps/everest/src/pages/db-cluster-details/cluster-overview/cluster-overview.tsx
+++ ui/apps/everest/src/pages/db-cluster-details/cluster-overview/cluster-overview.tsx
@@ -25,7 +25,7 @@ import { useDbClusterCredentials } from 'hooks/api/db-cluster/useCreateDbCluster
 import { useDbBackups } from 'hooks/api/backups/useBackups';
 import { DbEngineType } from 'shared-types/dbEngines.types';
 import { useRBACPermissions } from 'hooks/rbac';
-import { isProxy } from 'utils/db';
+import { isProxy, shouldDbActionsBeBlocked } from 'utils/db';
 
 export const ClusterOverview = () => {
   const { dbClusterName, namespace = '' } = useParams();
@@ -60,6 +60,7 @@ export const ClusterOverview = () => {
 
   const hasBackupsOrSchedules = schedules.length > 0 || backups.length > 0;
   const dbType = dbCluster?.spec.engine.type;
+  const actionsBlocked = shouldDbActionsBeBlocked(dbCluster.status?.status);
 
   const pitrEnabled =
     dbType === DbEngineType.POSTGRESQL
@@ -102,7 +103,7 @@ export const ClusterOverview = () => {
         dbCluster={dbCluster}
         sharding={dbCluster?.spec.sharding}
         loading={loadingCluster}
-        canUpdateDb={canUpdateDb}
+        canUpdate={canUpdateDb && !actionsBlocked}
       />
       {canReadBackups && (
         <BackupsDetails
