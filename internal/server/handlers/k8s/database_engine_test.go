da843fd0f5f52af71f2b038689f52c3ff29ea47c
Mayank Shah <mayank.shah@percona.com>
2025-04-03 16:22:24 +0530

    EVEREST-1975 | [bugfix] DBengine min version preflight checks (#1275)
    * [bugfix] Incorrect DBEngine version string during upgrade preflight checks
    
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- internal/server/handlers/k8s/database_engine_test.go
+++ internal/server/handlers/k8s/database_engine_test.go
@@ -111,6 +111,64 @@ func TestGetUpgradePreflightChecks(t *testing.T) {
 		assert.Equal(t, "Upgrade DB version to 0.5.0 or higher", pointer.Get(dbResult.Message))
 	})
 
+	t.Run("pending DB Engine upgrade [PG versioning]", func(t *testing.T) {
+		t.Parallel()
+
+		operatorVersion := "2.5.0"
+		targetVersion := "2.6.0"
+
+		// Setup mock
+		mockDBVersions := []string{"13.16", "14.12", "15.8", "16.4"}
+		versionService := versionservice.MockInterface{}
+		versionService.On(
+			"GetSupportedEngineVersions",
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+		).Return(mockDBVersions, nil)
+
+		dbs := []everestv1alpha1.DatabaseCluster{{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-db",
+				Namespace: "test-namespace",
+			},
+			Spec: everestv1alpha1.DatabaseClusterSpec{
+				Engine: everestv1alpha1.Engine{
+					Version: "12.19",
+				},
+			},
+		}}
+		engine := everestv1alpha1.DatabaseEngine{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-engine",
+				Namespace: "test-namespace",
+			},
+			Spec: everestv1alpha1.DatabaseEngineSpec{
+				Type: everestv1alpha1.DatabaseEnginePXC,
+			},
+			Status: everestv1alpha1.DatabaseEngineStatus{
+				OperatorVersion: operatorVersion,
+				PendingOperatorUpgrades: []everestv1alpha1.OperatorUpgrade{
+					{TargetVersion: targetVersion},
+				},
+			},
+		}
+
+		result, err := getUpgradePreflightChecksResult(ctx, dbs, upgradePreflightCheckArgs{
+			targetVersion:  targetVersion,
+			versionService: &versionService,
+			engine:         &engine,
+		})
+		require.NoError(t, err)
+		assert.NotNil(t, result)
+		assert.Equal(t, operatorVersion, result.currentVersion)
+		assert.Len(t, result.databases, 1)
+		dbResult := (result.databases)[0]
+		assert.Equal(t, "test-db", pointer.Get(dbResult.Name))
+		assert.Equal(t, api.UpgradeEngine, pointer.Get(dbResult.PendingTask))
+		assert.Equal(t, "Upgrade DB version to 13.16 or higher", pointer.Get(dbResult.Message))
+	})
+
 	t.Run("pending CRVersion update", func(t *testing.T) {
 		t.Parallel()
 		dbs := []everestv1alpha1.DatabaseCluster{{
