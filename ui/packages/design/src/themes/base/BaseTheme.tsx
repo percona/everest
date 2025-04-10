71ee2a2df96a1214151f53d0c19bf1ebe8c1eb7a
Diana Birsan <diana.birsan@3pillarglobal.com>
2025-04-10 11:08:56 +0300

    fix: focus state for icon buttons

--- ui/packages/design/src/themes/base/BaseTheme.tsx
+++ ui/packages/design/src/themes/base/BaseTheme.tsx
@@ -428,6 +428,22 @@ const baseThemeOptions = (mode: PaletteMode): ThemeOptions => ({
       defaultProps: {
         disableRipple: true,
       },
+      styleOverrides: {
+        root: {
+          '&.MuiIconButton-root:not(:focus-visible):focus': {
+            backgroundColor: 'transparent',
+          },
+          '&.MuiIconButton-root:focus-visible:focus': {
+            backgroundColor: 'rgba(18, 119, 227, 0.3)',
+          },
+          '&.MuiIconButton-root[aria-expanded="true"]': {
+            backgroundColor: 'rgba(18, 119, 227, 0.3)',
+          },
+          '&.MuiIconButton-root:focus-visible': {
+            backgroundColor: 'rgba(18, 119, 227, 0.3)',
+          },
+        },
+      },
     },
     MuiButton: {
       defaultProps: {
@@ -789,16 +805,6 @@ const baseThemeOptions = (mode: PaletteMode): ThemeOptions => ({
         root: () => ({
           '.MuiBadge-overlapCircular': {
             backgroundColor: 'transparent',
-          },
-          '.Mui-TableHeadCell-Content-Actions': {
-            '.MuiButtonBase-root': {
-              '&.MuiIconButton-root:focus-visible:focus': {
-                backgroundColor: 'rgba(18, 119, 227, 0.3)',
-              },
-              '&.MuiIconButton-root:not(:focus-visible):focus': {
-                backgroundColor: 'transparent',
-              },
-            },
           },
         }),
       },
