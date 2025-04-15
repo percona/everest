0548ef74d0dea0e371f3ede1ebbe5fa86a33769e
Fabio Silva <ffjs1993@gmail.com>
2025-04-15 15:44:10 +0100

    feat: policies table

--- ui/apps/everest/src/pages/settings/pod-scheduling-policies/pod-scheduling-policies.tsx
+++ ui/apps/everest/src/pages/settings/pod-scheduling-policies/pod-scheduling-policies.tsx
@@ -1,5 +1,29 @@
+import Add from '@mui/icons-material/Add';
+import { Button } from '@mui/material';
+import { Table } from '@percona/ui-lib';
+
 const PodSchedulingPolicies = () => {
-  return <></>;
+  return (
+    <Table
+      tableName="pod-scheduling-policies"
+      noDataMessage="No pod scheduling policies added"
+      data={[]}
+      columns={[]}
+      renderTopToolbarCustomActions={() => (
+        // TODO check if user has permission to create
+        <Button
+          size="small"
+          startIcon={<Add />}
+          data-testid="add-policy"
+          variant="outlined"
+          onClick={() => {}}
+          sx={{ display: 'flex' }}
+        >
+          Create policy
+        </Button>
+      )}
+    ></Table>
+  );
 };
 
 export default PodSchedulingPolicies;
