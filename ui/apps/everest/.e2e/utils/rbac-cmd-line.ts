613215d95c5ee3eb5382c85c00c12c223a67d4af
Fabio Silva <ffjs1993@gmail.com>
2025-05-13 10:59:49 +0100

    Revert "chore: patch k8s permissions RBAC without merge"
    This reverts commit 4e08edb73c3a7b0b1291a6b9da5ae37a0e787774.

--- ui/apps/everest/.e2e/utils/rbac-cmd-line.ts
+++ ui/apps/everest/.e2e/utils/rbac-cmd-line.ts
@@ -9,7 +9,7 @@ export const saveOldRBACPermissions = async () => {
 
 export const restoreOldRBACPermissions = async () => {
   const oldRbacFileContent = execSync(`cat ${OLD_RBAC_FILE}`).toString();
-  const command = `kubectl patch configmap/everest-rbac --namespace everest-system -p '{"data":${oldRbacFileContent}}'`;
+  const command = `kubectl patch configmap/everest-rbac --namespace everest-system --type merge -p '{"data":${oldRbacFileContent}}'`;
   execSync(command);
 
   return new Promise<void>((resolve) =>
@@ -22,7 +22,7 @@ export const restoreOldRBACPermissions = async () => {
 export const setRBACPermissionsK8S = async (
   permissions: [string, string, string][] = []
 ) => {
-  const command = `kubectl patch configmap/everest-rbac --namespace everest-system -p '{"data":{"enabled": "${permissions !== undefined}", "policy.csv":"g,${process.env.RBAC_USER},role:e2e-rbac-user\\n${permissions.map((p) => `p,role:e2e-rbac-user,${p.join(',')}`).join('\\n')}"}}'`;
+  const command = `kubectl patch configmap/everest-rbac --namespace everest-system --type merge -p '{"data":{"enabled": "${permissions !== undefined}", "policy.csv":"g,${process.env.RBAC_USER},role:e2e-rbac-user\\n${permissions.map((p) => `p,role:e2e-rbac-user,${p.join(',')}`).join('\\n')}"}}'`;
   execSync(command);
 
   // We need this to give time for the RBAC to be applied, or headless tests might fail for being too fast
