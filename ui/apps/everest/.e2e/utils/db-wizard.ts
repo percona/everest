8911c21152ad63ebe5faa1fa86f9fb9fb4f14f4f
Tomislav Plavcic <tplavcic@gmail.com>
2025-05-21 10:31:24 +0200

    Fix external access check in init-deploy test

--- ui/apps/everest/.e2e/utils/db-wizard.ts
+++ ui/apps/everest/.e2e/utils/db-wizard.ts
@@ -240,7 +240,9 @@ export const populateAdvancedConfig = async (
   await expect(combobox).toHaveValue(/.+/, { timeout: 5000 });
 
   if (externalAccess) {
-    await page.getByLabel('Enable External Access').check();
+    await page.getByTestId('switch-input-external-access')
+    .getByRole('checkbox')
+    .check();
     if (externalAccessSourceRange != '') {
       await page
         .getByTestId('text-input-source-ranges.0.source-range')
