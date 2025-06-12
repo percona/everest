076828f645bcc593744b513ae2b4255b0b40b2be
Fabio Silva <ffjs1993@gmail.com>
2025-06-12 17:18:59 +0100

    test: first passing test for pods scheduling

--- ui/apps/everest/.e2e/utils/policies.ts
+++ ui/apps/everest/.e2e/utils/policies.ts
@@ -1,4 +1,4 @@
-import { APIRequestContext, expect } from '@playwright/test';
+import { APIRequestContext, expect, Page } from '@playwright/test';
 import { getTokenFromLocalStorage } from './localStorage';
 
 export const createPodSchedulingPolicy = async (
@@ -49,4 +49,75 @@ export const getDefaultPodSchedulingPolicyNameForDbType = (
     default:
       throw new Error(`Unknown database type: ${dbType}`);
   }
+};
+
+export const createPodSchedulingPolicyWithValues = async (
+  page: Page,
+  policyName: string,
+  dbType: string
+) => {
+  const matchingPolicyType =
+    dbType === 'psmdb' ? 'MongoDB' : dbType === 'pxc' ? 'MySQL' : 'PostgreSQL';
+  await page.goto('/settings/pod-scheduling-policies');
+  await page.getByTestId('add-policy').waitFor();
+  await page.getByTestId('add-policy').click();
+  await page.getByTestId('text-input-name').fill(policyName);
+  await page.getByTestId('select-type-button').click();
+  await page
+    .getByRole('option', { name: matchingPolicyType, exact: true })
+    .click();
+  await page.getByTestId('form-dialog-create').click();
+};
+
+export const createRuleForPodSchedulingPolicyWithValues = async (
+  page: Page,
+  policyName: string,
+  component: 'proxy' | 'engine' | 'configServer',
+  type: 'podAntiAffinity' | 'podAffinity' | 'nodeAffinity',
+  priority: 'required' | 'preferred',
+  weight: number = 1,
+  topologyKey: string = 'kubernetes.io/hostname',
+  key?: string,
+  operator?: 'In' | 'NotIn' | 'Exists' | 'DoesNotExist',
+  values?: string
+) => {
+  await page.goto('/settings/pod-scheduling-policies');
+  await page.getByRole('table').waitFor();
+  await page
+    .locator('.MuiTableRow-root')
+    .filter({ hasText: policyName })
+    .click();
+  await page.getByRole('table').waitFor();
+  await page.getByRole('button', { name: 'Add rule' }).click();
+
+  await page.getByTestId('select-component-button').click();
+  await page.getByTestId(component).click();
+
+  await page.getByTestId('select-type-button').click();
+  await page.getByTestId(type).click();
+
+  await page.getByTestId(`toggle-button-${priority}`).click();
+
+  if (type !== 'nodeAffinity') {
+    await page.getByTestId('text-input-topology-key').fill(topologyKey);
+  }
+
+  if (priority === 'preferred') {
+    await page.getByTestId('text-input-weight').fill(weight.toString());
+  }
+
+  if (key) {
+    await page.getByTestId('text-input-key').fill(key);
+  }
+
+  if (operator) {
+    await page.getByTestId('select-operator-button').click();
+    await page.getByTestId(operator).click();
+  }
+
+  if (values) {
+    await page.getByTestId('text-input-values').fill(values);
+  }
+
+  await page.getByTestId('form-dialog-add').click();
 };
