e969a97ff56fce36831e4bc24936bfe139ba95e0
Mayank Shah <mayank.shah@percona.com>
2025-06-12 12:07:53 +0530

    fix rbac check for ListDataImportJobs
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- internal/server/handlers/rbac/data_import_job.go
+++ internal/server/handlers/rbac/data_import_job.go
@@ -2,6 +2,7 @@ package rbac
 
 import (
 	"context"
+	"errors"
 
 	everestv1alpha1 "github.com/percona/everest-operator/api/v1alpha1"
 	"github.com/percona/everest/pkg/rbac"
@@ -9,8 +10,21 @@ import (
 
 // ListDataImportJobs returns a list of DataImportJobs for the specified database clusters.
 func (h *rbacHandler) ListDataImportJobs(ctx context.Context, namespace, dbName string) (*everestv1alpha1.DataImportJobList, error) {
-	if err := h.enforce(ctx, rbac.ResourceDataImportJobs, rbac.ActionRead, rbac.ObjectName(namespace, dbName)); err != nil {
+	list, err := h.next.ListDataImportJobs(ctx, namespace, dbName)
+	if err != nil {
 		return nil, err
 	}
-	return h.next.ListDataImportJobs(ctx, namespace, dbName)
+	filtered := []everestv1alpha1.DataImportJob{}
+	for _, job := range list.Items {
+		if err := h.enforce(ctx, rbac.ResourceDataImportJobs,
+			rbac.ActionRead, rbac.ObjectName(namespace, job.Spec.TargetClusterName),
+		); errors.Is(err, ErrInsufficientPermissions) {
+			continue
+		} else if err != nil {
+			return nil, err
+		}
+		filtered = append(filtered, job)
+	}
+	list.Items = filtered
+	return list, nil
 }
