da091f4ebda3adbaaa3e9a5f96018041629c67bc
Diana Birsan <diana.birsan@3pillarglobal.com>
2025-07-01 13:13:38 +0300

    fix: update label

--- ui/apps/everest/src/pages/database-form/database-form-body/steps/import/import-step.messages.ts
+++ ui/apps/everest/src/pages/database-form/database-form-body/steps/import/import-step.messages.ts
@@ -13,6 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 export const Messages = {
-  pageTitle: 'Import info',
+  pageTitle: 'Import information',
   pageDescription: 'Provide the details of the file you want to import.',
 };
