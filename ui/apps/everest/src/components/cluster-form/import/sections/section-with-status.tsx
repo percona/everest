75f94bd914b1cbb55671b13cd90494d25f3ada73
Diana Birsan <diana.birsan@3pillarglobal.com>
2025-07-02 15:57:00 +0300

    fix: FormDialog cancel method

--- ui/apps/everest/src/components/cluster-form/import/sections/section-with-status.tsx
+++ ui/apps/everest/src/components/cluster-form/import/sections/section-with-status.tsx
@@ -2,7 +2,12 @@ import { Button, Typography } from '@mui/material';
 import { FormDialog } from 'components/form-dialog';
 import { ReactNode, useState } from 'react';
 import { Messages } from '../messages';
-import { useFormContext, useWatch } from 'react-hook-form';
+import {
+  useFormContext,
+  useWatch,
+  useForm,
+  FormProvider,
+} from 'react-hook-form';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import { z, ZodTypeDef } from 'zod';
 
@@ -22,34 +27,50 @@ const SectionFormWithStatus = ({
   const [openDialog, setOpenDialog] = useState(false);
   const { setValue, control, getValues, trigger } = useFormContext();
   const isSectionSaved = useWatch({ control, name: sectionSavedKey });
+  const methods = useForm({});
+
+  const handleOpenDialog = () => {
+    methods.reset(getValues());
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
+  const handleSubmit = (data: Record<string, unknown>) => {
+    setValue(sectionSavedKey, true);
+    Object.entries(data).forEach(([key, value]) => {
+      setValue(key, value);
+    });
+    setOpenDialog(false);
+    trigger();
+  };
 
   return (
     <>
-      <Button onClick={() => setOpenDialog(true)}>
+      <Button onClick={handleOpenDialog}>
         {isSectionSaved ? (
           <EditOutlinedIcon />
         ) : (
           <Typography>{Messages.fillDetails}</Typography>
         )}
       </Button>
-      <FormDialog
-        defaultValues={getValues()}
-        isOpen={openDialog}
-        closeModal={() => setOpenDialog(false)}
-        headerMessage={dialogTitle}
-        onSubmit={(data) => {
-          setValue(sectionSavedKey, true);
-          Object.entries(data).forEach(([key, value]) => {
-            setValue(key, value);
-          });
-          setOpenDialog(false);
-          trigger();
-        }}
-        schema={schema}
-        submitMessage="Save"
-      >
-        {children}
-      </FormDialog>
+      {openDialog && (
+        <FormProvider {...methods}>
+          <FormDialog
+            defaultValues={methods.getValues()}
+            isOpen={openDialog}
+            closeModal={handleCloseDialog}
+            headerMessage={dialogTitle}
+            onSubmit={handleSubmit}
+            schema={schema}
+            submitMessage="Save"
+          >
+            {children}
+          </FormDialog>
+        </FormProvider>
+      )}
     </>
   );
 };
