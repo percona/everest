895bf264d398ce1fce8707e2c2b7335b696beafa
Tikdev00 <tigran.papikyan@simbirsoft.com>
2025-07-02 16:48:38 +0400

    add button e2e tests fix-7

--- ui/apps/everest/.e2e/pr/db-cluster/db-wizard/create-db-cluster/errors-handling.e2e.ts
+++ ui/apps/everest/.e2e/pr/db-cluster/db-wizard/create-db-cluster/errors-handling.e2e.ts
@@ -66,6 +66,9 @@ test.describe('DB Cluster creation', () => {
     await page
       .getByTestId('text-input-source-ranges.0.source-range')
       .fill('invalid-ip');
+    await page
+      .getByTestId('text-input-source-ranges.1.source-range')
+      .fill('another-invalid-ip');
     await expect(
       page.getByTestId('preview-error-advanced-configurations')
     ).not.toBeVisible();
@@ -80,6 +83,7 @@ test.describe('DB Cluster creation', () => {
     await goToStep(page, 'resources');
     await page.getByTestId('text-input-memory').fill('1');
     await goToStep(page, 'advanced-configurations');
+    await page.getByTestId('delete-text-input-1-button').click();
     await page
       .getByTestId('text-input-source-ranges.0.source-range')
       .fill('192.168.1.1');
