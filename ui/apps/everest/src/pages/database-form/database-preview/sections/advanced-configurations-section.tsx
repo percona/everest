f1df808e33368656d46a5c529527cfc73d2dca25
Diana Birsan <diana.birsan@3pillarglobal.com>
2025-08-28 16:25:42 +0300

    fix: db summary data

--- ui/apps/everest/src/pages/database-form/database-preview/sections/advanced-configurations-section.tsx
+++ ui/apps/everest/src/pages/database-form/database-preview/sections/advanced-configurations-section.tsx
@@ -1,26 +1,36 @@
 import { PreviewContentText } from '../preview-section';
 import { AdvancedConfigurationType } from '../../database-form-schema.ts';
+import { ExposureMethod } from 'components/cluster-form/advanced-configuration/advanced-configuration.types.ts';
 
 export const AdvancedConfigurationsPreviewSection = ({
-  externalAccess,
+  exposureMethod,
+  loadBalancerConfigName,
   engineParametersEnabled,
   engineParameters,
   storageClass,
   podSchedulingPolicyEnabled,
   podSchedulingPolicy,
-}: AdvancedConfigurationType) => (
-  <>
-    <PreviewContentText text={`Storage class: ${storageClass ?? ''}`} />
-    <PreviewContentText
-      text={`External access ${externalAccess ? 'enabled' : 'disabled'}`}
-    />
-    {engineParametersEnabled && engineParameters && (
-      <PreviewContentText text="Database engine parameters set" />
-    )}
-    {podSchedulingPolicyEnabled && podSchedulingPolicy && (
+}: AdvancedConfigurationType) => {
+  const isExternalAccessEnabled =
+    exposureMethod === ExposureMethod.LoadBalancer;
+
+  return (
+    <>
+      <PreviewContentText text={`Storage class: ${storageClass ?? ''}`} />
       <PreviewContentText
-        text={`Pod scheduling policy: ${podSchedulingPolicy}`}
+        text={`Ext. access: ${isExternalAccessEnabled ? 'enabled' : 'disabled'}`}
       />
-    )}
-  </>
-);
+      {isExternalAccessEnabled && (
+        <PreviewContentText text={`Config name: ${loadBalancerConfigName}`} />
+      )}
+      {engineParametersEnabled && engineParameters && (
+        <PreviewContentText text="Database engine parameters set" />
+      )}
+      {podSchedulingPolicyEnabled && podSchedulingPolicy && (
+        <PreviewContentText
+          text={`Pod scheduling policy: ${podSchedulingPolicy}`}
+        />
+      )}
+    </>
+  );
+};
