ccc509b60d73ed9400242a9cf9419765b954b27b
Fábio Silva <ffjs1993@gmail.com>
2025-09-01 14:48:06 +0100

    Fix tests (#1616)

--- ui/apps/everest/src/components/cluster-form/advanced-configuration/constants.ts
+++ ui/apps/everest/src/components/cluster-form/advanced-configuration/constants.ts
@@ -4,10 +4,11 @@ import { LoadBalancerConfig } from 'shared-types/loadbalancer.types';
 export const SELECT_WIDTH = '200px';
 
 export const emtpyConfig: LoadBalancerConfig = {
-  apiVersion: '',
-  kind: '',
   metadata: {
     name: EMPTY_LOAD_BALACNER_CONFIGURATION,
+    resourceVersion: '',
+    finalizers: [],
+    generation: 0,
   },
   spec: { annotations: {} },
 };
