a4f0319bcdc8694ff39a023d86a7cda8c157bc84
Diana Birsan <diana.birsan@3pillarglobal.com>
2025-09-09 14:01:57 +0300

    fix: default on edit mode after LBC creation

--- ui/apps/everest/src/pages/settings/policies/load-balancer-configuration/configurations-list/configurations-list.tsx
+++ ui/apps/everest/src/pages/settings/policies/load-balancer-configuration/configurations-list/configurations-list.tsx
@@ -51,7 +51,14 @@ const LoadBalancerConfigurationList = () => {
         queryClient.invalidateQueries({
           queryKey: ['load-balancer-configs'],
         });
-        navigate(`/settings/policies/load-balancer-configuration/${data.name}`);
+        navigate(
+          `/settings/policies/load-balancer-configuration/${data.name}`,
+          {
+            state: {
+              creation: true,
+            },
+          }
+        );
       },
     });
   };
