77036f2c71cc998ae78c3f29c09c1f0d9a49e2c4
Fabio Silva <ffjs1993@gmail.com>
2025-10-07 12:54:50 +0100

    feat: table with add button

--- ui/apps/everest/src/pages/settings/policies/split-horizon/split-horizon.tsx
+++ ui/apps/everest/src/pages/settings/policies/split-horizon/split-horizon.tsx
@@ -1,5 +1,55 @@
+import { Add } from '@mui/icons-material';
+import { Button } from '@mui/material';
+import { Table } from '@percona/ui-lib';
+import { useMemo } from 'react';
+
 const SplitHorizon = () => {
-  return <div>SplitHorizon</div>;
+  const columns = useMemo(
+    () => [
+      {
+        header: 'Domain name',
+        accessorKey: 'domain',
+      },
+    ],
+    []
+  );
+
+  const data = useMemo(
+    () => [
+      {
+        domain: 'domain',
+      },
+      {
+        domain: 'domain',
+      },
+      {
+        domain: 'domain',
+      },
+      {
+        domain: 'domain',
+      },
+    ],
+    []
+  );
+  return (
+    <Table
+      tableName="split-horizon"
+      columns={columns}
+      data={data}
+      renderTopToolbarCustomActions={() => (
+        <Button
+          variant="contained"
+          size="medium"
+          onClick={() => {}}
+          data-testid="add-config-button"
+          sx={{ display: 'flex' }}
+          startIcon={<Add />}
+        >
+          Create configuration
+        </Button>
+      )}
+    />
+  );
 };
 
 export default SplitHorizon;
